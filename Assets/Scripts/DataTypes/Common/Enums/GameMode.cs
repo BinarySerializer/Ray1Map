581030361b2b1954c44748fa59ebd9b68c6ab577
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-03-04 14:08:40 +0100

    Added more commented info to game mode enum

--- Assets/Scripts/DataTypes/Common/Enums/GameMode.cs
+++ Assets/Scripts/DataTypes/Common/Enums/GameMode.cs
@@ -1,27 +1,40 @@
 ﻿using System.ComponentModel;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace R1Engine
 {
     /// <summary>
     /// The available game modes
     /// </summary>
-    public enum GameMode 
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum GameMode
     {
         [Description("Rayman 1 (PS1)")]
         RaymanPS1,
 
+        // NOTE: Already works with level files, but world file is different - uses raw 1555 for tiles
         //[Description("Rayman 1 (PS1 - Japanese)")]
         //RaymanPS1JP,
+
+        // NOTE: More or less same format as PS1, but split into multiple files. DTA is the event block.
+        //[Description("Rayman 1 (Saturn)")]
+        //RaymanSaturn,
 
         [Description("Rayman 1 (PC)")]
         RaymanPC,
 
-        //[Description("Rayman Designer (PC)")]
+        //[Description("Rayman Designer (PC - 24 New Levels)")]
         //RaymanDesignerPC,
 
+        //[Description("Rayman Designer (PC - Custom Maps)")]
+        //RaymanDesignerCustomMapsPC,
+
+        // NOTE: Same as Designer
         //[Description("Rayman by his Fans (PC)")]
         //RaymanByHisFansPC,
 
+        // NOTE: Same as Designer
         //[Description("Rayman 60 Levels (PC)")]
         //Rayman60LevelsPC,
     }
