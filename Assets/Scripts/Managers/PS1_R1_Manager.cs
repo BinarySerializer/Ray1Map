5c992414a3c8c407cafc489ebcbb05815b1541b6
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-03-05 09:22:25 +0100

    Improved PS1 world file parsing

--- Assets/Scripts/Managers/PS1_R1_Manager.cs
+++ Assets/Scripts/Managers/PS1_R1_Manager.cs
@@ -79,43 +79,24 @@ namespace R1Engine
             return Directory.EnumerateFiles(worldPath, "*.XXX", SearchOption.TopDirectoryOnly).Count(x => Path.GetFileNameWithoutExtension(x)?.Length == 5);
         }
 
-        // TODO: Replace this entire function with the class PS1_R1_WorldFile to store the data in
+        /// <summary>
+        /// Reads the tile set for the specified world
+        /// </summary>
+        /// <param name="basePath">The base game path</param>
+        /// <param name="world">The world</param>
+        /// <returns>The tile set</returns>
         public Common_Tileset ReadTileSet(string basePath, World world)
         {
-            var fileStream = new FileStream(Path.Combine(GetWorldFolderPath(basePath, world), $"{GetWorldName(world)}.XXX"), FileMode.Open);
-            byte[] file = new byte[fileStream.Length];
-            fileStream.Read(file, 0, (int)fileStream.Length);
-            fileStream.Close();
+            // Get the file name
+            var fileName = Path.Combine(GetWorldFolderPath(basePath, world), $"{GetWorldName(world)}.XXX");
 
-            int off_tiles = BitConverter.ToInt32(file, 0x18);
-            int off_palette = BitConverter.ToInt32(file, 0x1C);
-            int off_assign = BitConverter.ToInt32(file, 0x20);
-            int off_end = BitConverter.ToInt32(file, 0x24);
-
-            // Palettes
-            var palettes = new List<Color[]>();
-            for (int i = off_palette; i < off_assign;)
-            {
-                var p = new Color[256];
-                for (int c = 0; c < 256; c++, i += 2)
-                {
-                    uint colour16 = BitConverter.ToUInt16(file, i); // ABGR 1555
-                    byte r = (byte)((colour16 & 0x1F) << 3);
-                    byte g = (byte)(((colour16 & 0x3E0) >> 5) << 3);
-                    byte b = (byte)(((colour16 & 0x7C00) >> 10) << 3);
-
-                    if (r + g + b > 0)
-                        p[c] = new Color((float)r / 255, (float)g / 255, (float)b / 255, 1);
-                    else
-                        p[c] = new Color(0, 0, 0, 0);
-                }
-                palettes.Add(p);
-            }
+            // Read the file
+            var worldFile = FileFactory.Read<PS1_R1_WorldFile>(fileName);
 
             int tile = 0;
-            int tileCount = off_end - off_assign;
-            int width = 256;
-            int height = (off_palette - off_tiles) / width;
+            int tileCount = worldFile.PaletteIndexTable.Length;
+            const int width = 256;
+            int height = (worldFile.TilesIndexTable.Length) / width;
             Color[] pixels = new Color[width * height];
 
             for (int yB = 0; yB < height; yB += 16)
@@ -125,8 +106,10 @@ namespace R1Engine
             {
                 if (tile >= tileCount)
                     goto End;
+
                 int pixel = x + xB + (y + yB) * width;
-                pixels[pixel] = palettes[file[off_assign + tile]][file[off_tiles + pixel]];
+
+                pixels[pixel] = worldFile.ColorPalettes[worldFile.PaletteIndexTable[tile]][worldFile.TilesIndexTable[pixel]].GetColor();
             }
             End:
             Texture2D tex = new Texture2D(width, height, TextureFormat.RGBA32, false)
