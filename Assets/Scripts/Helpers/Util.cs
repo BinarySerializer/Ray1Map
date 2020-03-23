17ee54d03e1670dab8cf5e692c0733721373ad54
byvar <39142447+byvar@users.noreply.github.com>
2020-03-23 05:45:42 +0100

    Util code to decrypt Rayman Classic files. Maybe move this to its own manager when you add support for it :)

--- Assets/Scripts/Helpers/Util.cs
+++ Assets/Scripts/Helpers/Util.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using UnityEngine;
 
@@ -65,6 +66,61 @@ namespace R1Engine {
 			}
 
 			return Result.ToString();
+		}
+
+		public static string DecryptSparkName(string str) {
+			string decrypted = "";
+			string key = "UBICOSMOS";
+			string keyL = "ubicosmos";
+			int keyIndex = 0;
+			for (int i = 0; i < str.Length; i++) {
+				int c = str[i];
+				int k = key[keyIndex];
+				if (c >= 'a' && c <= 'z') {
+					int nc = (c - k - 6) % 26;
+					decrypted += (char)(int)(nc + (int)'a');
+					keyIndex = (keyIndex + 1) % key.Length;
+				} else if (c >= 'A' && c <= 'Z') {
+					int nc = (c - k + 26) % 26;
+					decrypted += (char)(int)(nc + (int)'A');
+					keyIndex = (keyIndex + 1) % key.Length;
+				} else {
+					decrypted += (char)c;
+					//keyIndex = 0;
+				}
+			}
+			return decrypted;
+		}
+
+		public static void DecryptRaymanClassic(string basePath) {
+			IEnumerable<string> files = Directory.EnumerateFiles(basePath, "*.spd*", SearchOption.AllDirectories).Select(f => f.Substring(basePath.Length));
+			foreach (string f in files) {
+				string dirname = Path.GetDirectoryName(f);
+				string filename = Path.GetFileName(f);
+				string filenameNoExt = filename.Substring(0, filename.Length - 4);
+				string decFileName = Util.DecryptSparkName(filenameNoExt);
+				MonoBehaviour.print(filenameNoExt + " - " + decFileName);
+				using (Stream s = FileSystem.GetFileReadStream(basePath + f)) {
+					using (Reader reader = new Reader(s)) {
+						byte[] bytes = reader.ReadBytes((int)s.Length);
+						if (bytes.Length > 0) {
+							using (Rijndael r = Rijndael.Create()) {
+								r.Key = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1, 10 };
+								r.IV = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+								using (MemoryStream ms = new MemoryStream()) {
+									using (CryptoStream cs = new CryptoStream(ms, r.CreateDecryptor(), CryptoStreamMode.Write)) {
+										cs.Write(bytes, 0, bytes.Length);
+									}
+									byte[] dec = ms.ToArray();
+									Util.ByteArrayToFile(basePath + "Decrypted/" + dirname + "/" + decFileName, dec);
+								}
+							}
+						} else {
+							Util.ByteArrayToFile(basePath + "Decrypted/" + dirname + "/" + decFileName, new byte[0]);
+						}
+					}
+				}
+			}
 		}
 	}
 }
