b76e4f7d0f2a38ef4964185a0616ff282b72dfc7
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-03-28 14:20:43 +0100

    Added commented out game mode selection for EU demo

--- Assets/Scripts/Settings/GameModeSelection.cs
+++ Assets/Scripts/Settings/GameModeSelection.cs
@@ -15,6 +15,9 @@ namespace R1Engine
         //[GameMode(EngineVersion.RayPS1, Game.Rayman1, "Rayman 1 (PS1 - EU)", typeof(PS1_R1_Manager))]
         //RaymanPS1EU,
 
+        //[GameMode(EngineVersion.RayPS1, Game.Rayman1, "Rayman 1 (PS1 - EU Demo)", typeof(PS1_R1_Manager))]
+        //RaymanPS1EUDemo,
+
         [GameMode(EngineVersion.RayPS1JP, Game.Rayman1, "Rayman 1 (PS1 - JP)", typeof(PS1_R1JP_Manager))]
         RaymanPS1Japan,
 
