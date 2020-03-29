c06d08e22db9addf0e71c2454c9fa1a0bb4f9fd6
Raymanni <ejp.heikkila@gmail.com>
2020-03-29 19:36:39 +0300

    Saving level as PNG

--- Assets/Scripts/LevelEditor/LevelMainController.cs
+++ Assets/Scripts/LevelEditor/LevelMainController.cs
@@ -25,6 +25,9 @@ namespace R1Engine
         // Reference to the background ting
         public MeshFilter backgroundTint;
 
+        // Render camera things
+        public Camera renderCamera;
+        public Texture2D tex;
 
         public async Task LoadLevelAsync(IGameManager manager, Serialize.Context context) 
         {
@@ -65,6 +68,7 @@ namespace R1Engine
 
                 mo.SetIndices(new int[] { 0, 1, 2, 3 }, MeshTopology.Quads, 0);
                 backgroundTint.sharedMesh = mo;
+
             }
         }
 
@@ -73,6 +77,42 @@ namespace R1Engine
                 Settings.GetGameManager.SaveLevel(serializeContext, currentLevel);
             }
             Debug.Log("Saved.");
+        }
+
+        public void ConvertLevelToPNG() {
+            //Hide unused links and palette swappers etc
+            foreach(var e in currentLevel.Events) {
+                if (e.Type == EventType.TYPE_PALETTE_SWAPPER) {
+                    e.gameObject.SetActive(false);
+                }
+                e.ChangeLinksVisibility(true);
+                if (e.LinkID == 0) {
+                    e.lineRend.enabled = false;
+                    e.linkCube.gameObject.SetActive(false);
+                }
+            }
+
+            RenderTexture renderTex = new RenderTexture(currentLevel.Width*16, currentLevel.Height*16, 24);
+            renderCamera.targetTexture = renderTex;
+            //Set camera pos
+            renderCamera.transform.position = new Vector3((currentLevel.Width) / 2f, -(currentLevel.Height) / 2f, renderCamera.transform.position.z);
+            renderCamera.orthographicSize = (currentLevel.Height / 2f);
+            renderCamera.Render();
+
+            //Save to picture
+            RenderTexture.active = renderTex;
+
+            tex = new Texture2D(renderTex.width, renderTex.height, TextureFormat.RGBA32, false);
+            tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+            tex.Apply();
+
+            var bytes = tex.EncodeToPNG();
+            File.WriteAllBytes($"E:/RayExports/{ Settings.World}{ Settings.Level:00}.png", bytes);
+
+            Destroy(tex);
+            RenderTexture.active = null;
+
+            Debug.Log("Level saved as PNG");
         }
     }
 }
