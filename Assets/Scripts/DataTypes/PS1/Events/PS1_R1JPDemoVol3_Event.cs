399cfc1afb71c358bf599f0afb2beca4a1922687
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-03-31 16:13:44 +0200

    Parsed JP vol3 event pointers

--- Assets/Scripts/DataTypes/PS1/Events/PS1_R1JPDemoVol3_Event.cs
+++ Assets/Scripts/DataTypes/PS1/Events/PS1_R1JPDemoVol3_Event.cs
@@ -1,20 +1,30 @@
 ﻿namespace R1Engine
 {
+    // TODO: Merge with PS1_R1_Event once all values have been parsed
     /// <summary>
     /// Event data for Rayman 1 (PS1 - Japan Demo Vol3)
     /// </summary>
     public class PS1_R1JPDemoVol3_Event : R1Serializable
     {
-        // Graphics?
+        /// <summary>
+        /// The pointer to the image descriptors
+        /// </summary>
+        public Pointer ImageDescriptorsPointer { get; set; }
+
+        /// <summary>
+        /// The pointer to the animation descriptors
+        /// </summary>
+        public Pointer AnimDescriptorsPointer { get; set; }
+
         public Pointer UnknownPointer1 { get; set; }
+
+        // ETA?
         public Pointer UnknownPointer2 { get; set; }
-        public Pointer UnknownPointer3 { get; set; }
 
-        // LabelOffsets?
-        public Pointer UnknownPointer4 { get; set; }
-
-        // Commands?
-        public Pointer UnknownPointer5 { get; set; }
+        /// <summary>
+        /// Pointer to the event commands
+        /// </summary>
+        public Pointer CommandsPointer { get; set; }
 
         public byte[] Unk1 { get; set; }
 
@@ -22,6 +32,7 @@
 
         public ushort YPosition { get; set; }
 
+        // Last byte might be anim desc count
         public byte[] Unk2 { get; set; }
 
         /// <summary>
@@ -31,11 +42,11 @@
         public override void SerializeImpl(SerializerObject s)
         {
             // Serialize pointers
+            ImageDescriptorsPointer = s.SerializePointer(ImageDescriptorsPointer, name: nameof(ImageDescriptorsPointer));
+            AnimDescriptorsPointer = s.SerializePointer(AnimDescriptorsPointer, name: nameof(AnimDescriptorsPointer));
             UnknownPointer1 = s.SerializePointer(UnknownPointer1, name: nameof(UnknownPointer1));
             UnknownPointer2 = s.SerializePointer(UnknownPointer2, name: nameof(UnknownPointer2));
-            UnknownPointer3 = s.SerializePointer(UnknownPointer3, name: nameof(UnknownPointer3));
-            UnknownPointer4 = s.SerializePointer(UnknownPointer4, name: nameof(UnknownPointer4));
-            UnknownPointer5 = s.SerializePointer(UnknownPointer5, name: nameof(UnknownPointer5));
+            CommandsPointer = s.SerializePointer(CommandsPointer, name: nameof(CommandsPointer));
 
             // Serialize values
             Unk1 = s.SerializeArray(Unk1, 46, name: nameof(Unk1));
