859bdb01d18c7d6041f6d6dc6b0e8064b55fab8d
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-04-22 21:19:06 +0200

    R2 PS1: Parsed more event values

--- Assets/Scripts/DataTypes/PS1/Events/PS1_R2Demo_Event.cs
+++ Assets/Scripts/DataTypes/PS1/Events/PS1_R2Demo_Event.cs
@@ -1,6 +1,5 @@
 ﻿namespace R1Engine
 {
-    // TODO: Merge with PS1_R1_Event once all values have been parsed
     /// <summary>
     /// Event data for Rayman 2 (PS1 - Demo)
     /// </summary>
@@ -21,14 +20,42 @@
         // Pointer1 leads to more pointers
         public Pointer UnkPointer3 { get; set; }
 
-        // Always 0
-        public uint Unk1 { get; set; }
+        // Always 0 in file - gets set to a pointer during runtime
+        public uint RuntimePointer1 { get; set; }
 
         public ushort XPosition { get; set; }
         
         public ushort YPosition { get; set; }
 
+        public byte[] Unk1 { get; set; }
+
+        // Dev pointer in file - gets set to a pointer during runtime
+        public uint RuntimePointer2 { get; set; }
+
+        // Always 0 in file - gets set to a pointer during runtime
+        public uint RuntimePointer3 { get; set; }
+
         public byte[] Unk2 { get; set; }
+
+        // Some runtime offset for the current sprite animation - setting to 0 places sprite in its origin point during runtime
+        public ushort RuntimeOffset1 { get; set; }
+        public ushort RuntimeOffset2 { get; set; }
+
+        // Always 0 in file
+        public byte[] RuntimeBytes1 { get; set; }
+
+        /// <summary>
+        /// The current animation frame during runtime
+        /// </summary>
+        public byte RuntimeCurrentAnimFrame { get; set; }
+
+        // Always 0 in file - probably animation related
+        public byte[] RuntimeBytes2 { get; set; }
+
+        // The layer to appear on - only effects visual so not the map's z-index?
+        public byte Layer { get; set; }
+
+        public byte[] Unk3 { get; set; }
 
         /// <summary>
         /// Handles the data serialization
@@ -48,11 +75,28 @@
             UnkPointer2 = s.SerializePointer(UnkPointer2, name: nameof(UnkPointer2));
             UnkPointer3 = s.SerializePointer(UnkPointer3, name: nameof(UnkPointer3));
 
-            Unk1 = s.Serialize<uint>(Unk1, name: nameof(Unk1));
+            RuntimePointer1 = s.Serialize<uint>(RuntimePointer1, name: nameof(RuntimePointer1));
+
             XPosition = s.Serialize<ushort>(XPosition, name: nameof(XPosition));
             YPosition = s.Serialize<ushort>(YPosition, name: nameof(YPosition));
 
-            Unk2 = s.SerializeArray(Unk2, 76, name: nameof(Unk2));
+            Unk1 = s.SerializeArray(Unk1, 24, name: nameof(Unk1));
+
+            RuntimePointer2 = s.Serialize<uint>(RuntimePointer2, name: nameof(RuntimePointer2));
+            RuntimePointer3 = s.Serialize<uint>(RuntimePointer3, name: nameof(RuntimePointer3));
+
+            Unk2 = s.SerializeArray(Unk2, 8, name: nameof(Unk2));
+
+            RuntimeOffset1 = s.Serialize<ushort>(RuntimeOffset1, name: nameof(RuntimeOffset1));
+            RuntimeOffset2 = s.Serialize<ushort>(RuntimeOffset2, name: nameof(RuntimeOffset2));
+
+            RuntimeBytes1 = s.SerializeArray(RuntimeBytes1, 8, name: nameof(RuntimeBytes1));
+            RuntimeCurrentAnimFrame = s.Serialize<byte>(RuntimeCurrentAnimFrame, name: nameof(RuntimeCurrentAnimFrame));
+            RuntimeBytes2 = s.SerializeArray(RuntimeBytes2, 4, name: nameof(RuntimeBytes2));
+
+            Layer = s.Serialize<byte>(Layer, name: nameof(Layer));
+
+            Unk3 = s.SerializeArray(Unk3, 18, name: nameof(Unk3));
 
             /*s.DoAt(UnkPointer3, () => {
                 Pointer ptr = s.SerializePointer(null, name: "test");
