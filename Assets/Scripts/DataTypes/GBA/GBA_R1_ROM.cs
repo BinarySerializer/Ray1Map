5352458521f505ece458da8f97294fea29d5a366
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-05-14 15:03:32 +0200

    GBA: Attempting to parse event data directly from rom

--- Assets/Scripts/DataTypes/GBA/GBA_R1_ROM.cs
+++ Assets/Scripts/DataTypes/GBA/GBA_R1_ROM.cs
@@ -10,6 +10,7 @@
         /// </summary>
         public GBA_R1_Level[] Levels { get; set; }
 
+
         /// <summary>
         /// The background vignette data
         /// </summary>
@@ -19,23 +20,18 @@
 
         public GBA_R1_WorldMapVignette WorldMapVignette { get; set; }
 
+
         /// <summary>
         /// The sprite palettes. The game uses the same 16 palettes (with 16 colors) for every sprite in the game. During runtime this gets copied to 0x05000200.
         /// </summary>
         public ARGB1555Color[] SpritePalettes { get; set; }
 
-        // Pointers seem to lead to another pointer-array which in term leads to structs like this {ImageBufferPointer, (uint)Length} - maybe not? Only matches some.
         public Pointer[] UnkLevelPointerArray1 { get; set; }
-
-        // Pointers seem to lead to another pointer-array which in term leads to 28-byte long structs which begin with 2 pointers
         public Pointer[] UnkLevelPointerArray2 { get; set; }
-
-        // Pointers lead to 6 initial bytes followed by 28-byte long structs with two pointers in the middle
         public Pointer[] UnkLevelPointerArray3 { get; set; }
-
-        // Indexes to something. It almost matches the music tracks.
         public uint[] UnkLevelDwordArray { get; set; }
 
+      
         /// <summary>
         /// Handles the data serialization
         /// </summary>
@@ -67,8 +63,155 @@
             s.DoAt(new Pointer(pointerTable[GBA_R1_ROMPointer.UnkLevelPointerArray2], this.Offset.file), () => UnkLevelPointerArray2 = s.SerializePointerArray(UnkLevelPointerArray2, levelCount, name: nameof(UnkLevelPointerArray2)));
             s.DoAt(new Pointer(pointerTable[GBA_R1_ROMPointer.UnkLevelPointerArray3], this.Offset.file), () => UnkLevelPointerArray3 = s.SerializePointerArray(UnkLevelPointerArray3, levelCount, name: nameof(UnkLevelPointerArray3)));
             s.DoAt(new Pointer(pointerTable[GBA_R1_ROMPointer.UnkLevelDwordArray], this.Offset.file), () => UnkLevelDwordArray = s.SerializeArray<uint>(UnkLevelDwordArray, levelCount, name: nameof(UnkLevelDwordArray)));
+
+            Test(s);
+        }
+        
+        public void Test(SerializerObject s)
+        {
+            // For every level...
+            for (int i = 0; i < Levels.Length; i++)
+            {
+                // Get values
+                var p1 = UnkLevelPointerArray1[i];
+                var p2 = UnkLevelPointerArray2[i];
+                var p3 = UnkLevelPointerArray3[i];
+                var v = UnkLevelDwordArray[i];
+
+                byte[] v7values = null;
+                s.DoAt(p3, () => v7values = s.SerializeArray<byte>(v7values, v, name: nameof(v7values)));
+
+                for (int j = 0; j < v; j++)
+                {
+                    var v7 = v7values[j];
+
+                    var eventStruct1CurrentPointer = p2 + (4 * j);
+                    var eventStruct2CurrentPointer = p1 + (4 * j);
+
+                    Pointer pr2 = null;
+                    s.DoAt(eventStruct1CurrentPointer, () => pr2 = s.SerializePointer(pr2, name: nameof(pr2)));
+
+                    Pointer pr1 = null;
+
+                    s.DoAt(eventStruct2CurrentPointer, () => pr1 = s.SerializePointer(pr1, name: nameof(pr1)));
+
+                    for (int v5 = 0; v5 < v7; v5++)
+                    {
+                        for (int eventIndex = 0; eventIndex < 255; eventIndex++)
+                        {
+                            GBA_R1_EventGraphicsData eventGraphicsData = null;
+                            GBA_R1_EventData eventData = null;
+
+                            s.DoAt(pr1, () => eventGraphicsData = s.SerializeObject<GBA_R1_EventGraphicsData>(eventGraphicsData, name: nameof(eventGraphicsData)));
+
+                            s.DoAt(pr2, () => eventData = s.SerializeObject<GBA_R1_EventData>(eventData, name: nameof(eventData)));
+
+                            pr2 += 28;
+                        }
+                    }
+                }
+            }
         }
     }
+
+    // From sub_6438
+    public class GBA_R1_EventData : R1Serializable
+    {
+        public Pointer ETAPointer { get; set; }
+
+        public Pointer CommandsPointer { get; set; }
+
+        public ushort XPosition { get; set; }
+        public ushort YPosition { get; set; }
+
+        public ushort Layer { get; set; }
+
+        //*(_WORD*) (2 * eventIndex + v8549200) = *(_WORD*) (eventStruct1CurrentPointer + 14); - linkindex???
+        public ushort SomeIndex { get; set; }
+
+        public byte Etat { get; set; }
+        public byte SubEtat { get; set; }
+        public byte OffsetBX { get; set; }
+        public byte OffsetBY { get; set; }
+        public byte OffsetHY { get; set; }
+
+        // *(_BYTE*)(eventOffsetInMemory + 112) = *(_BYTE*)(eventOffsetInMemory + 112) & 0xFE | *(_BYTE*)(eventStruct1CurrentPointer + 21) & 1;
+        public byte SomeFlag { get; set; }
+
+        public byte FollowSprite { get; set; }
+        public byte HitPoints { get; set; }
+
+        /// <summary>
+        /// Handles the data serialization
+        /// </summary>
+        /// <param name="s">The serializer object</param>
+        public override void SerializeImpl(SerializerObject s)
+        {
+            ETAPointer = s.SerializePointer(ETAPointer, name: nameof(ETAPointer));
+            CommandsPointer = s.SerializePointer(CommandsPointer, name: nameof(CommandsPointer));
+            XPosition = s.Serialize<ushort>(XPosition, name: nameof(XPosition));
+            YPosition = s.Serialize<ushort>(YPosition, name: nameof(YPosition));
+            Layer = s.Serialize<ushort>(Layer, name: nameof(Layer));
+            SomeIndex = s.Serialize<ushort>(SomeIndex, name: nameof(SomeIndex));
+            Etat = s.Serialize<byte>(Etat, name: nameof(Etat));
+            SubEtat = s.Serialize<byte>(SubEtat, name: nameof(SubEtat));
+            OffsetBX = s.Serialize<byte>(OffsetBX, name: nameof(OffsetBX));
+            OffsetBY = s.Serialize<byte>(OffsetBY, name: nameof(OffsetBY));
+            OffsetHY = s.Serialize<byte>(OffsetHY, name: nameof(OffsetHY));
+            SomeFlag = s.Serialize<byte>(SomeFlag, name: nameof(SomeFlag));
+            FollowSprite = s.Serialize<byte>(FollowSprite, name: nameof(FollowSprite));
+            HitPoints = s.Serialize<byte>(HitPoints, name: nameof(HitPoints));
+        }
+    }
+
+    public class GBA_R1_EventGraphicsData : R1Serializable
+    {
+        // sub_50BA4((_BYTE*)eventPointer, 0, 116u);
+
+        public Pointer ImageBufferPointer { get; set; }
+
+        // Gets set to offset 30 in memory, which gets overwritten by the position
+        public uint ImageBufferSize { get; set; }
+
+        public Pointer ImageDescriptorsPointer { get; set; }
+
+        // *(_WORD*)(eventOffsetInMemory + 50) = sub_4EC90(*(_DWORD*)(structOffset + 12), 0xCu);
+        public uint ImageDescriptorCount { get; set; }
+
+        public Pointer ETAPointer { get; set; }
+
+        // Usually between 1-3
+        public uint Unk { get; set; }
+
+        public Pointer AnimDescriptorsPointer { get; set; }
+
+        public uint AnimDescriptorCount { get; set; }
+
+        // Some event values gets reset here:
+        /*
+         
+            *(_BYTE*)(eventOffsetInMemory + 88) = 0;
+            *(_WORD*)(eventOffsetInMemory + 72) = 0;
+            *(_WORD*)(eventOffsetInMemory + 56) = -1;
+            *(_WORD*)(eventOffsetInMemory + 66) = (unsigned int)&dword_2710;
+             
+             */
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            ImageBufferPointer = s.SerializePointer(ImageBufferPointer, name: nameof(ImageBufferPointer));
+            ImageBufferSize = s.Serialize<uint>(ImageBufferSize, name: nameof(ImageBufferSize));
+            ImageDescriptorsPointer = s.SerializePointer(ImageDescriptorsPointer, name: nameof(ImageDescriptorsPointer));
+            ImageDescriptorCount = s.Serialize<uint>(ImageDescriptorCount, name: nameof(ImageDescriptorCount));
+            ETAPointer = s.SerializePointer(ETAPointer, name: nameof(ETAPointer));
+            Unk = s.Serialize<uint>(Unk, name: nameof(Unk));
+            AnimDescriptorsPointer = s.SerializePointer(AnimDescriptorsPointer, name: nameof(AnimDescriptorsPointer));
+            AnimDescriptorCount = s.Serialize<uint>(AnimDescriptorCount, name: nameof(AnimDescriptorCount));
+        }
+    }
+
+
+
 
     /*
 
@@ -101,6 +244,18 @@
     LOADING + CREDITS SCREENS:
 
     Palettes don't seem to exist in the rom - compressed?
+
+
+    MEMORY LOCATIONS:
+
+    0x02030394 - current world
+    0x0202E5F0 - current level
+    0x0202A2DA - some array where first two bytes are used to compare level and world somehow
+
+
+    ROM LOCATIONS TO PARSE:
+
+    0x081539A4 - index table for level offsets based on world in global level array
 
      */
 }
