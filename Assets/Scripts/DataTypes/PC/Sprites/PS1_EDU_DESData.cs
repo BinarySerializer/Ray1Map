41ded2259208dc8a4c2bf526e62d997a6c8b35b0
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-05-29 17:45:26 +0200

    PS1 EDU: Parsed more DES data

--- Assets/Scripts/DataTypes/PC/Sprites/PS1_EDU_DESData.cs
+++ Assets/Scripts/DataTypes/PC/Sprites/PS1_EDU_DESData.cs
@@ -9,17 +9,20 @@
 
         public uint AnimationDescriptorsPointer { get; set; }
 
+        // Always 0?
         public byte[] Unk1 { get; set; }
 
-        // ushort/uint?
-        public byte ImageDescriptorsCount { get; set; }
+        public uint ImageBufferLength { get; set; }
 
+        // Always 0?
         public byte[] Unk2 { get; set; }
 
-        // ushort/uint?
-        public byte AnimationDescriptorsCount { get; set; }
+        public uint ImageDescriptorsCount { get; set; }
 
+        // Always 0?
         public byte[] Unk3 { get; set; }
+
+        public uint AnimationDescriptorsCount { get; set; }
 
         /// <summary>
         /// Handles the data serialization
@@ -29,11 +32,12 @@
         {
             ImageDescriptorsPointer = s.Serialize<uint>(ImageDescriptorsPointer, name: nameof(ImageDescriptorsPointer));
             AnimationDescriptorsPointer = s.Serialize<uint>(AnimationDescriptorsPointer, name: nameof(AnimationDescriptorsPointer));
-            Unk1 = s.SerializeArray<byte>(Unk1, 60, name: nameof(Unk1));
-            ImageDescriptorsCount = s.Serialize<byte>(ImageDescriptorsCount, name: nameof(ImageDescriptorsCount));
-            Unk2 = s.SerializeArray<byte>(Unk2, 63, name: nameof(Unk2));
-            AnimationDescriptorsCount = s.Serialize<byte>(AnimationDescriptorsCount, name: nameof(AnimationDescriptorsCount));
-            Unk3 = s.SerializeArray<byte>(Unk3, 3, name: nameof(Unk3));
+            Unk1 = s.SerializeArray<byte>(Unk1, 32, name: nameof(Unk1));
+            ImageBufferLength = s.Serialize<uint>(ImageBufferLength, name: nameof(ImageBufferLength));
+            Unk2 = s.SerializeArray<byte>(Unk2, 24, name: nameof(Unk2));
+            ImageDescriptorsCount = s.Serialize<uint>(ImageDescriptorsCount, name: nameof(ImageDescriptorsCount));
+            Unk3 = s.SerializeArray<byte>(Unk3, 60, name: nameof(Unk3));
+            AnimationDescriptorsCount = s.Serialize<uint>(AnimationDescriptorsCount, name: nameof(AnimationDescriptorsCount));
         }
     }
 }
