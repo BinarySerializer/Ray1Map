09a86db48fbbaa6c41ca1659717b1f4ea62bae92
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-06-04 08:50:06 +0200

    Jaguar: Started filling out img descriptor class

--- Assets/Scripts/DataTypes/Common/Serializable/Sprites/Common_ImageDescriptor.cs
+++ Assets/Scripts/DataTypes/Common/Serializable/Sprites/Common_ImageDescriptor.cs
@@ -44,7 +44,7 @@
         /// </summary>
         public ushort ImageType { get; set; }
 
-        // Some of these are hitbox related - one is probably a flag for if the sprite is a dummy sprite (garbage pixels)
+        // Some of these are hitbox related
         public byte Unknown2 { get; set; }
         public byte Unknown3 { get; set; }
         public byte Unknown4 { get; set; }
@@ -55,54 +55,98 @@
         public byte ImageOffsetInPageY { get; set; }
         public ushort Unknown6 { get; set; }
 
+        public byte Jag_Byte05 { get; set; }
+        public ushort Jag_Ushort06 { get; set; }
+        public ushort Jag_Ushort08 { get; set; }
+        public byte[] Jag_Bytes0A { get; set; }
+
         /// <summary>
         /// Handles the data serialization
         /// </summary>
         /// <param name="s">The serializer object</param>
         public override void SerializeImpl(SerializerObject s) 
         {
-            if (s.GameSettings.Game != Game.Rayman2) {
-                ImageBufferOffset = s.Serialize<uint>(ImageBufferOffset, name: nameof(ImageBufferOffset));
-            }
-
-            if (s.GameSettings.MajorEngineVersion == MajorEngineVersion.PS1)
+            if (s.GameSettings.MajorEngineVersion == MajorEngineVersion.Jaguar)
             {
-                if (s.Context.Settings.Game == Game.Rayman2) {
-                    OuterWidth = s.Serialize<byte>((byte)OuterWidth, name: nameof(OuterWidth));
-                    OuterHeight = s.Serialize<byte>((byte)OuterHeight, name: nameof(OuterHeight));
-                    InnerWidth = s.Serialize<byte>(InnerWidth, name: nameof(InnerWidth));
-                    InnerHeight = s.Serialize<byte>(InnerHeight, name: nameof(InnerHeight));
-                    Unknown3 = s.Serialize<byte>(Unknown3, name: nameof(Unknown3));
-                    Unknown4 = s.Serialize<byte>(Unknown4, name: nameof(Unknown4));
-                    ImageOffsetInPageX = s.Serialize<byte>(ImageOffsetInPageX, name: nameof(ImageOffsetInPageX));
-                    ImageOffsetInPageY = s.Serialize<byte>(ImageOffsetInPageY, name: nameof(ImageOffsetInPageY));
-                    PaletteInfo = s.Serialize<ushort>(PaletteInfo, name: nameof(PaletteInfo));
-                    TexturePageInfo = s.Serialize<ushort>(TexturePageInfo, name: nameof(TexturePageInfo));
-                }
-                else if (s.Context.Settings.EngineVersion == EngineVersion.RayPS1JP || s.Context.Settings.EngineVersion == EngineVersion.RayPS1JPDemoVol3 || s.Context.Settings.EngineVersion == EngineVersion.RayPS1JPDemoVol6)
+                // TODO: Is the fourth byte always 0?
+                ImageBufferOffset = s.Serialize<uint>(ImageBufferOffset << 8, name: nameof(ImageBufferOffset)) >> 8;
+
+                Jag_Byte05 = s.Serialize<byte>(Jag_Byte05, name: nameof(Jag_Byte05));
+                Jag_Ushort06 = s.Serialize<ushort>(Jag_Ushort06, name: nameof(Jag_Ushort06));
+                OuterHeight = (ushort)(Jag_Ushort06 << 6);
+                Jag_Ushort08 = s.Serialize<ushort>(Jag_Ushort08, name: nameof(Jag_Ushort08));
+                OuterWidth = s.Serialize<byte>((byte)OuterWidth, name: nameof(OuterWidth));
+
+                Jag_Bytes0A = s.SerializeArray<byte>(Jag_Bytes0A, 5, name: nameof(Jag_Bytes0A));
+                Index = s.Serialize<byte>((byte)Index, name: nameof(Index));
+            }
+            else
+            {
+                if (s.GameSettings.Game != Game.Rayman2)
+                    ImageBufferOffset = s.Serialize<uint>(ImageBufferOffset, name: nameof(ImageBufferOffset));
+
+                if (s.GameSettings.MajorEngineVersion == MajorEngineVersion.PS1)
                 {
-                    Index = s.Serialize<ushort>(Index, name: nameof(Index));
-                    ImageType = s.Serialize<ushort>(ImageType, name: nameof(ImageType));
-                    OuterWidth = s.Serialize<ushort>(OuterWidth, name: nameof(OuterWidth));
-                    OuterHeight = s.Serialize<ushort>(OuterHeight, name: nameof(OuterHeight));
-                    Unknown2 = (byte)s.Serialize<ushort>(Unknown2, name: nameof(Unknown2));
+                    if (s.Context.Settings.Game == Game.Rayman2)
+                    {
+                        OuterWidth = s.Serialize<byte>((byte)OuterWidth, name: nameof(OuterWidth));
+                        OuterHeight = s.Serialize<byte>((byte)OuterHeight, name: nameof(OuterHeight));
+                        InnerWidth = s.Serialize<byte>(InnerWidth, name: nameof(InnerWidth));
+                        InnerHeight = s.Serialize<byte>(InnerHeight, name: nameof(InnerHeight));
+                        Unknown3 = s.Serialize<byte>(Unknown3, name: nameof(Unknown3));
+                        Unknown4 = s.Serialize<byte>(Unknown4, name: nameof(Unknown4));
+                        ImageOffsetInPageX = s.Serialize<byte>(ImageOffsetInPageX, name: nameof(ImageOffsetInPageX));
+                        ImageOffsetInPageY = s.Serialize<byte>(ImageOffsetInPageY, name: nameof(ImageOffsetInPageY));
+                        PaletteInfo = s.Serialize<ushort>(PaletteInfo, name: nameof(PaletteInfo));
+                        TexturePageInfo = s.Serialize<ushort>(TexturePageInfo, name: nameof(TexturePageInfo));
+                    }
+                    else if (s.Context.Settings.EngineVersion == EngineVersion.RayPS1JP || s.Context.Settings.EngineVersion == EngineVersion.RayPS1JPDemoVol3 || s.Context.Settings.EngineVersion == EngineVersion.RayPS1JPDemoVol6)
+                    {
+                        Index = s.Serialize<ushort>(Index, name: nameof(Index));
+                        ImageType = s.Serialize<ushort>(ImageType, name: nameof(ImageType));
+                        OuterWidth = s.Serialize<ushort>(OuterWidth, name: nameof(OuterWidth));
+                        OuterHeight = s.Serialize<ushort>(OuterHeight, name: nameof(OuterHeight));
+                        Unknown2 = (byte)s.Serialize<ushort>(Unknown2, name: nameof(Unknown2));
+                    }
+                    else if (s.GameSettings.EngineVersion == EngineVersion.RaySaturn)
+                    {
+                        Index = s.Serialize<ushort>(Index, name: nameof(Index));
+
+                        // ?
+                        ImageType = s.Serialize<ushort>(ImageType, name: nameof(ImageType));
+
+                        OuterWidth = s.Serialize<ushort>(OuterWidth, name: nameof(OuterWidth));
+                        OuterHeight = s.Serialize<ushort>(OuterHeight, name: nameof(OuterHeight));
+
+                        // Unsure below here...
+                        PaletteInfo = s.Serialize<ushort>(PaletteInfo, name: nameof(PaletteInfo));
+
+                        Unknown2 = (byte)s.Serialize<ushort>(Unknown2, name: nameof(Unknown2));
+                    }
+                    else
+                    {
+                        Index = s.Serialize<byte>((byte)Index, name: nameof(Index));
+                        OuterWidth = s.Serialize<byte>((byte)OuterWidth, name: nameof(OuterWidth));
+                        OuterHeight = s.Serialize<byte>((byte)OuterHeight, name: nameof(OuterHeight));
+                        InnerWidth = s.Serialize<byte>(InnerWidth, name: nameof(InnerWidth));
+                        InnerHeight = s.Serialize<byte>(InnerHeight, name: nameof(InnerHeight));
+                        Unknown2 = s.Serialize<byte>(Unknown2, name: nameof(Unknown2));
+                    }
+
+                    if (s.GameSettings.EngineVersion != EngineVersion.RaySaturn
+                        && s.GameSettings.Game != Game.Rayman2)
+                    {
+                        Unknown3 = s.Serialize<byte>(Unknown3, name: nameof(Unknown3));
+                        Unknown4 = s.Serialize<byte>(Unknown4, name: nameof(Unknown4));
+                        PaletteInfo = s.Serialize<ushort>(PaletteInfo, name: nameof(PaletteInfo));
+
+                        TexturePageInfo = s.Serialize<ushort>(TexturePageInfo, name: nameof(TexturePageInfo));
+                        ImageOffsetInPageX = s.Serialize<byte>(ImageOffsetInPageX, name: nameof(ImageOffsetInPageX));
+                        ImageOffsetInPageY = s.Serialize<byte>(ImageOffsetInPageY, name: nameof(ImageOffsetInPageY));
+                        Unknown6 = s.Serialize<ushort>(Unknown6, name: nameof(Unknown6));
+                    }
                 }
-                else if (s.GameSettings.EngineVersion == EngineVersion.RaySaturn)
-                {
-                    Index = s.Serialize<ushort>(Index, name: nameof(Index));
-
-                    // ?
-                    ImageType = s.Serialize<ushort>(ImageType, name: nameof(ImageType));
-
-                    OuterWidth = s.Serialize<ushort>(OuterWidth, name: nameof(OuterWidth));
-                    OuterHeight = s.Serialize<ushort>(OuterHeight, name: nameof(OuterHeight));
-
-                    // Unsure below here...
-                    PaletteInfo = s.Serialize<ushort>(PaletteInfo, name: nameof(PaletteInfo));
-
-                    Unknown2 = (byte)s.Serialize<ushort>(Unknown2, name: nameof(Unknown2));
-                }
-                else
+                else if (s.GameSettings.MajorEngineVersion == MajorEngineVersion.PC)
                 {
                     Index = s.Serialize<byte>((byte)Index, name: nameof(Index));
                     OuterWidth = s.Serialize<byte>((byte)OuterWidth, name: nameof(OuterWidth));
@@ -110,30 +154,9 @@
                     InnerWidth = s.Serialize<byte>(InnerWidth, name: nameof(InnerWidth));
                     InnerHeight = s.Serialize<byte>(InnerHeight, name: nameof(InnerHeight));
                     Unknown2 = s.Serialize<byte>(Unknown2, name: nameof(Unknown2));
-                }
-
-                if (s.GameSettings.EngineVersion != EngineVersion.RaySaturn
-                    && s.GameSettings.Game != Game.Rayman2) {
                     Unknown3 = s.Serialize<byte>(Unknown3, name: nameof(Unknown3));
                     Unknown4 = s.Serialize<byte>(Unknown4, name: nameof(Unknown4));
-                    PaletteInfo = s.Serialize<ushort>(PaletteInfo, name: nameof(PaletteInfo));
-
-                    TexturePageInfo = s.Serialize<ushort>(TexturePageInfo, name: nameof(TexturePageInfo));
-                    ImageOffsetInPageX = s.Serialize<byte>(ImageOffsetInPageX, name: nameof(ImageOffsetInPageX));
-                    ImageOffsetInPageY = s.Serialize<byte>(ImageOffsetInPageY, name: nameof(ImageOffsetInPageY));
-                    Unknown6 = s.Serialize<ushort>(Unknown6, name: nameof(Unknown6));
                 }
-            }
-            else if (s.GameSettings.MajorEngineVersion == MajorEngineVersion.PC)
-            {
-                Index = s.Serialize<byte>((byte)Index, name: nameof(Index));
-                OuterWidth = s.Serialize<byte>((byte)OuterWidth, name: nameof(OuterWidth));
-                OuterHeight = s.Serialize<byte>((byte)OuterHeight, name: nameof(OuterHeight));
-                InnerWidth = s.Serialize<byte>(InnerWidth, name: nameof(InnerWidth));
-                InnerHeight = s.Serialize<byte>(InnerHeight, name: nameof(InnerHeight));
-                Unknown2 = s.Serialize<byte>(Unknown2, name: nameof(Unknown2));
-                Unknown3 = s.Serialize<byte>(Unknown3, name: nameof(Unknown3));
-                Unknown4 = s.Serialize<byte>(Unknown4, name: nameof(Unknown4));
             }
         }
     }
