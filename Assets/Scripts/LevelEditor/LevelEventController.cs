d6b2c585c326fee17aa52b8e9a82361235764191
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-07-08 09:29:56 +0200

    Disabled logging edits

--- Assets/Scripts/LevelEditor/LevelEventController.cs
+++ Assets/Scripts/LevelEditor/LevelEventController.cs
@@ -66,7 +66,7 @@ namespace R1Engine
         public bool hasLoaded;
 
         public BaseEditorManager EditorManager => Controller.obj.levelController.EditorManager;
-        public bool LogModifications => true;
+        public bool LogModifications => false;
 
         #endregion
 
