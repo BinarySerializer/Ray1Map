336ca1a292b37a3bf46d0c558ef211cb54a89000
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-08-08 18:08:54 +0200

    GBA R3: GBA_R3_Level is parallax info for BG_0

---
+++ Assets/Scripts/DataTypes/GBA/GBA_R3_BGParallax.cs
@@ -0,0 +1,43 @@
+﻿namespace R1Engine
+{
+    // First  level is at 0x082E7288
+    // Second level is at 0x08362544
+    // 0x03000e20 has pointer to this struct for the current level during runtime
+
+    // Always 128 bytes long - appears right before BG_0 for a map
+    public class GBA_R3_BGParallax : R1Serializable
+    {
+        public bool Unk_00 { get; set; }
+
+        public byte Unk_01 { get; set; }
+
+        // This includes the palette
+        public byte TilemapIndex { get; set; }
+        
+        public byte Unk_03 { get; set; }
+
+        public byte TileLayersCount { get; set; }
+        public byte UnkIndexesCount { get; set; }
+        
+        public byte[] TileLayerIDs { get; set; }
+        public byte[] UnkIndexes { get; set; }
+
+        // More data (offset table?) - part of the same struct?
+
+        /// <summary>
+        /// Handles the data serialization
+        /// </summary>
+        /// <param name="s">The serializer object</param>
+        public override void SerializeImpl(SerializerObject s)
+        {
+            Unk_00 = s.Serialize<bool>(Unk_00, name: nameof(Unk_00));
+            Unk_01 = s.Serialize<byte>(Unk_01, name: nameof(Unk_01));
+            TilemapIndex = s.Serialize<byte>(TilemapIndex, name: nameof(TilemapIndex));
+            Unk_03 = s.Serialize<byte>(Unk_03, name: nameof(Unk_03));
+            TileLayersCount = s.Serialize<byte>(TileLayersCount, name: nameof(TileLayersCount));
+            UnkIndexesCount = s.Serialize<byte>(UnkIndexesCount, name: nameof(UnkIndexesCount));
+            TileLayerIDs = s.SerializeArray<byte>(TileLayerIDs, 4, name: nameof(TileLayerIDs));
+            UnkIndexes = s.SerializeArray<byte>(UnkIndexes, 6, name: nameof(UnkIndexes));
+        }
+    }
+}
--- Assets/Scripts/DataTypes/GBA/GBA_R3_ROM.cs
+++ Assets/Scripts/DataTypes/GBA/GBA_R3_ROM.cs
@@ -4,6 +4,7 @@ namespace R1Engine
 {
     public class GBA_R3_ROM : GBA_ROM
     {
+        // TODO: Only around the first half of the items are obj! What are the rest?
         public uint ObjBlocksOffsetTableCount { get; set; }
         public uint[] ObjBlocksOffsetTable { get; set; }
         public GBA_R3_MapObjBlock[] ObjBlocks { get; set; }
@@ -15,6 +16,8 @@ namespace R1Engine
         // Contains general info about levels, but not anything map related
         public GBA_R3_LevelMapInfo[] LevelInfo { get; set; }
 
+
+        public GBA_R3_BGParallax BG_0_Parallax { get; set; }
 
         // The background (usually clouds, the sky etc.)
         public GBA_R3_MapBlock BG_0 { get; set; }
@@ -68,6 +71,7 @@ namespace R1Engine
             LevelInfo = s.DoAt(pointerTable[GBA_R3_Pointer.LevelInfo], () => s.SerializeObjectArray<GBA_R3_LevelMapInfo>(LevelInfo, 65, name: nameof(LevelInfo)));
 
             // Serialize current level maps
+            BG_0_Parallax = s.DoAt(Offset + 0x2E7288, () => s.SerializeObject<GBA_R3_BGParallax>(BG_0_Parallax, name: nameof(BG_0_Parallax)));
             BG_0 = s.DoAt(Offset + 0x2E7308, () => s.SerializeObject<GBA_R3_MapBlock>(BG_0, name: nameof(BG_0)));
             BG_1 = s.DoAt(Offset + 0x2E8094, () => s.SerializeObject<GBA_R3_MapBlock>(BG_1, name: nameof(BG_1)));
             BG_2 = s.DoAt(Offset + 0x2E86DC, () => s.SerializeObject<GBA_R3_MapBlock>(BG_2, name: nameof(BG_2)));
@@ -83,47 +87,6 @@ namespace R1Engine
     }
 
     // TODO: Move to separate files
-
-    // FUN_080c5378 in proto uses this to load map layers
-
-    // First  level is at 0x082E7288
-    // Second level is at 0x08362544
-    // 0x03000e20 has pointer to this struct for the current level during runtime
-
-    // Always 128 bytes long - appears right before BG_0 for a map
-    public class GBA_R3_Level : R1Serializable
-    {
-        // This determines how the level gets loaded (false == normal map, true == ?)
-        public bool Unk_00 { get; set; }
-
-        public byte Unk_01 { get; set; }
-        public byte Unk_02 { get; set; }
-        public byte Unk_03 { get; set; }
-
-        public byte TileLayersCount { get; set; }
-        public byte UnkIndexesCount { get; set; }
-        
-        public byte[] TileLayerIDs { get; set; }
-        public byte[] UnkIndexes { get; set; }
-
-        // More data - part of the same struct?
-
-        /// <summary>
-        /// Handles the data serialization
-        /// </summary>
-        /// <param name="s">The serializer object</param>
-        public override void SerializeImpl(SerializerObject s)
-        {
-            Unk_00 = s.Serialize<bool>(Unk_00, name: nameof(Unk_00));
-            Unk_01 = s.Serialize<byte>(Unk_01, name: nameof(Unk_01));
-            Unk_02 = s.Serialize<byte>(Unk_02, name: nameof(Unk_02));
-            Unk_03 = s.Serialize<byte>(Unk_03, name: nameof(Unk_03));
-            TileLayersCount = s.Serialize<byte>(TileLayersCount, name: nameof(TileLayersCount));
-            UnkIndexesCount = s.Serialize<byte>(UnkIndexesCount, name: nameof(UnkIndexesCount));
-            TileLayerIDs = s.SerializeArray<byte>(TileLayerIDs, 4, name: nameof(TileLayerIDs));
-            UnkIndexes = s.SerializeArray<byte>(UnkIndexes, 6, name: nameof(UnkIndexes));
-        }
-    }
 
     public class GBA_R3_MapObjBlock : R1Serializable
     {
