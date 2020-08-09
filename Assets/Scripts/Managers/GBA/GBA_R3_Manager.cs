c89e0198af94a39f2452aa7ef3c5119ebeccf6a8
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-08-09 16:33:18 +0200

    GBA R3: Added tools for exporting blocks & vignette

--- Assets/Scripts/Managers/GBA/GBA_R3_Manager.cs
+++ Assets/Scripts/Managers/GBA/GBA_R3_Manager.cs
@@ -1,6 +1,7 @@
 ﻿using R1Engine.Serialize;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -19,7 +20,168 @@ namespace R1Engine
 
         public virtual string GetROMFilePath => $"ROM.gba";
 
-        public GameAction[] GetGameActions(GameSettings settings) => new GameAction[0];
+        public GameAction[] GetGameActions(GameSettings settings) => new GameAction[]
+        {
+            new GameAction("Export Compressed Blocks", false, true, (input, output) => ExportAllCompressedBlocksAsync(settings, output)),
+            new GameAction("Export Vignette", false, true, (input, output) => ExtractVignetteAsync(settings, output)),
+        };
+
+        // TODO: Find the way the game gets the vignette offsets and find remaining vignettes
+        public async Task ExtractVignetteAsync(GameSettings settings, string outputDir)
+        {
+            // Create a context
+            using (var context = new Context(settings))
+            {
+                // Load the ROM
+                await LoadFilesAsync(context);
+
+                // Get the file
+                var file = context.GetFile(GetROMFilePath);
+
+                // Get the deserialize
+                var s = context.Deserializer;
+
+                const int vigCount = 20;
+                const uint palOffset = 0x0B37C0;
+                const uint vigOffset = 0x20ED94;
+
+                var palettes = new ARGB1555Color[vigCount][];
+
+                for (int i = 0; i < vigCount; i++)
+                    palettes[i] = s.DoAt((file.StartPointer + palOffset) + (512 * i), () => s.SerializeObjectArray<ARGB1555Color>(default, 256));
+
+                // Go to vignette offset
+                s.Goto(file.StartPointer + vigOffset);
+
+                // Export every vignette
+                for (int i = 0; i < vigCount; i++)
+                {
+                    // Get the offset
+                    var offset = s.CurrentPointer;
+
+                    // Decode data
+                    byte[] data = null;
+                    s.DoEncoded(new LZSSEncoder(), () => data = s.SerializeArray<byte>(default, s.CurrentLength));
+
+                    // Create a texture
+                    var tex = new Texture2D(240, 160);
+
+                    var palIndex = i;
+
+                    // Palettes for 6 and 7 are swapped
+                    if (palIndex == 6)
+                        palIndex = 7;
+                    else if (palIndex == 7)
+                        palIndex = 6;
+
+                    // Set pixels
+                    for (int y = 0; y < tex.height; y++)
+                    {
+                        for (int x = 0; x < tex.width; x++)
+                        {
+                            var c = palettes[palIndex][data[y * tex.width + x]].GetColor();
+
+                            // Remove transparency
+                            c.a = 1;
+
+                            // Set pixel and reverse height
+                            tex.SetPixel(x, tex.height - y - 1, c);
+                        }
+                    }
+
+                    tex.Apply();
+
+                    // Export
+                    Util.ByteArrayToFile(Path.Combine(outputDir, $"Vig_{i}_0x{offset.AbsoluteOffset:X8}.png"), tex.EncodeToPNG());
+
+                    // Align
+                    s.Align();
+                }
+            }
+        }
+
+        public async Task ExportAllCompressedBlocksAsync(GameSettings settings, string outputDir)
+        {
+            // Create a context
+            using (var context = new Context(settings))
+            {
+                // Load the ROM
+                await LoadFilesAsync(context);
+
+                // Get the file
+                var file = (GBAMemoryMappedFile)context.GetFile(GetROMFilePath);
+
+                // Get the deserialize
+                var s = context.Deserializer;
+
+                // Keep track of blocks
+                var blocks = new List<Tuple<long, long, int>>();
+
+                // Enumerate every fourth byte (compressed blocks are always aligned to 4)
+                for (int i = 0; i < file.Length; i += 4)
+                {
+                    // Go to the offset
+                    s.Goto(file.StartPointer + i);
+
+                    // Check for compression header
+                    if (s.Serialize<byte>(default) == 0x10)
+                    {
+                        // Get the decompressed size
+                        var decompressedSizeValue = s.SerializeArray<byte>(default, 3);
+                        Array.Resize(ref decompressedSizeValue, 4);
+                        var decompressedSize = BitConverter.ToUInt32(decompressedSizeValue, 0);
+
+                        // Skip if the decompressed size is too low
+                        if (decompressedSize <= 32) 
+                            continue;
+                        
+                        // Go back to the offset
+                        s.Goto(file.StartPointer + i);
+
+                        // Attempt to decompress
+                        try
+                        {
+                            byte[] data = null;
+
+                            s.DoEncoded(new LZSSEncoder(), () => data = s.SerializeArray<byte>(default, s.CurrentLength));
+
+                            // Make sure we got some data
+                            if (data != null && data.Length > 32)
+                            {
+                                Util.ByteArrayToFile(Path.Combine(outputDir, $"Block_0x{(file.StartPointer + i).AbsoluteOffset:X8}.dat"), data);
+
+                                blocks.Add(new Tuple<long, long, int>((file.StartPointer + i).AbsoluteOffset, s.CurrentPointer - (file.StartPointer + i), data.Length));
+                            }
+                        }
+                        catch
+                        {
+                            // Ignore exceptions...
+                        }
+                    }
+                }
+
+                var log = new List<string>();
+
+                for (int i = 0; i < blocks.Count; i++)
+                {
+                    var (offset, compressedSize, size) = blocks[i];
+
+                    var end = offset + compressedSize;
+
+                    log.Add($"0x{offset:X8} - 0x{end:X8} (0x{compressedSize:X8} - 0x{size:X8}) - ");
+
+                    if (i != blocks.Count - 1)
+                    {
+                        var dif = blocks[i + 1].Item1 - end;
+
+                        if (dif >= 4)
+                            log.Add($"0x{end:X8} - 0x{end + dif:X8} (0x{dif:X8})              - ");
+                    }
+                }
+
+                File.WriteAllLines(Path.Combine(outputDir, "blocks_log.txt"), log);
+            }
+        }
 
         public async Task<BaseEditorManager> LoadAsync(Context context, bool loadTextures)
         {
