526d13177c9cf985a03c7fae3c52e3be76c2ed29
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-09-16 14:31:05 +0200

    PC: Corrected mem value names

--- Assets/Scripts/DataTypes/Rayman1/PC/Version/R1_PC_VersionMemoryInfo.cs
+++ Assets/Scripts/DataTypes/Rayman1/PC/Version/R1_PC_VersionMemoryInfo.cs
@@ -4,14 +4,12 @@
     {
         public uint TailleMainMemTmp { get; set; }
         public uint TailleMainMemFix { get; set; }
-        public uint TailleMainMemSprite { get; set; }
         public uint TailleMainMemWorld { get; set; }
         public uint TailleMainMemLevel { get; set; }
-        public uint SamplesTableMemory { get; set; }
-
-        // Some fixed data
-        public uint Unk7 { get; set; }
-        public uint Unk8 { get; set; }
+        public uint TailleMainMemSprite { get; set; }
+        public uint TailleMainMemSamplesTable { get; set; }
+        public uint TailleMainMemEdit { get; set; }
+        public uint TailleMainMemSaveEvent { get; set; }
 
         /// <summary>
         /// Handles the data serialization
@@ -25,25 +23,25 @@
             TailleMainMemFix = s.Serialize<uint>(TailleMainMemFix, name: nameof(TailleMainMemFix));
             s.Log($"{nameof(TailleMainMemFix)}: {TailleMainMemFix << 10} bytes");
             
-            TailleMainMemSprite = s.Serialize<uint>(TailleMainMemSprite, name: nameof(TailleMainMemSprite));
-            s.Log($"{nameof(TailleMainMemSprite)}: {TailleMainMemSprite << 10} bytes");
-            
             TailleMainMemWorld = s.Serialize<uint>(TailleMainMemWorld, name: nameof(TailleMainMemWorld));
             s.Log($"{nameof(TailleMainMemWorld)}: {TailleMainMemWorld << 10} bytes");
             
             TailleMainMemLevel = s.Serialize<uint>(TailleMainMemLevel, name: nameof(TailleMainMemLevel));
             s.Log($"{nameof(TailleMainMemLevel)}: {TailleMainMemLevel << 10} bytes");
             
-            SamplesTableMemory = s.Serialize<uint>(SamplesTableMemory, name: nameof(SamplesTableMemory));
-            s.Log($"{nameof(SamplesTableMemory)}: {SamplesTableMemory << 10} bytes");
+            TailleMainMemSprite = s.Serialize<uint>(TailleMainMemSprite, name: nameof(TailleMainMemSprite));
+            s.Log($"{nameof(TailleMainMemSprite)}: {TailleMainMemSprite << 10} bytes");
+            
+            TailleMainMemSamplesTable = s.Serialize<uint>(TailleMainMemSamplesTable, name: nameof(TailleMainMemSamplesTable));
+            s.Log($"{nameof(TailleMainMemSamplesTable)}: {TailleMainMemSamplesTable << 10} bytes");
 
             if (s.GameSettings.EngineVersion == EngineVersion.R1_PC_Kit)
             {
-                Unk7 = s.Serialize<uint>(Unk7, name: nameof(Unk7));
-                s.Log($"{nameof(Unk7)}: {Unk7 << 10} bytes");
+                TailleMainMemEdit = s.Serialize<uint>(TailleMainMemEdit, name: nameof(TailleMainMemEdit));
+                s.Log($"{nameof(TailleMainMemEdit)}: {TailleMainMemEdit << 10} bytes");
                 
-                Unk8 = s.Serialize<uint>(Unk8, name: nameof(Unk8));
-                s.Log($"{nameof(Unk8)}: {Unk8 << 10} bytes");
+                TailleMainMemSaveEvent = s.Serialize<uint>(TailleMainMemSaveEvent, name: nameof(TailleMainMemSaveEvent));
+                s.Log($"{nameof(TailleMainMemSaveEvent)}: {TailleMainMemSaveEvent << 10} bytes");
             }
         }
     }
