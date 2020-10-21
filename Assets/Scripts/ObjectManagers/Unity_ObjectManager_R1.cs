44552d0e615d2d2374aaba382e326db7749b5a18
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-10-21 15:30:02 +0200

    R1: Fix for randomized link chain

--- Assets/Scripts/ObjectManagers/Unity_ObjectManager_R1.cs
+++ Assets/Scripts/ObjectManagers/Unity_ObjectManager_R1.cs
@@ -263,19 +263,22 @@ namespace R1Engine
             // Set frames for linked events
             for (int i = 0; i < level.EventData.Count; i++)
             {
+                // Recreated from allocateOtherPosts
                 var baseEvent = (Unity_Object_R1)level.EventData[i];
+                var linkedIndex = LinkTable[i];
 
-                if (baseEvent.EventData.Type.UsesRandomFrame())
+                if (baseEvent.EventData.Type.UsesRandomFrame() && i != linkedIndex)
                 {
-                    // Recreated from allocateOtherPosts
                     var index = 0;
-                    var linkedIndex = LinkTable[i];
 
                     do
                     {
                         index++;
 
-                        ((Unity_Object_R1)level.EventData[linkedIndex]).ForceFrame = (byte)((baseEvent.ForceFrame + index) % ((Unity_Object_R1)level.EventData[linkedIndex]).CurrentAnimation?.Frames.Length ?? 1);
+                        var e = (Unity_Object_R1)level.EventData[linkedIndex];
+                        e.ForceFrame = (byte)((baseEvent.ForceFrame + index) % (e.CurrentAnimation?.Frames.Length ?? 1));
+                        e.XPosition = (short)(baseEvent.EventData.XPosition + 32 * index * (baseEvent.EventData.HitPoints - 2));
+                        e.YPosition = baseEvent.YPosition;
 
                         linkedIndex = LinkTable[linkedIndex];
                     } while (i != linkedIndex);
