edd09b6b63371143db8b8fa25f9fce0e70797f9a
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-10-21 14:34:03 +0200

    R2: Filled out some more ZDC arrays

---
+++ Assets/Scripts/DataTypes/Rayman1/PS1/Level/R1_R2ZDCUnkData.cs
@@ -0,0 +1,17 @@
+﻿namespace R1Engine
+{
+    public class R1_R2ZDCUnkData : R1Serializable
+    {
+        public ushort Data1 { get; set; }
+        public byte Data2 { get; set; }
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            s.SerializeBitValues<ushort>(bitFunc =>
+            {
+                Data1 = (ushort)bitFunc(Data1, 12, name: nameof(Data1));
+                Data2 = (byte)bitFunc(Data2, 4, name: nameof(Data2));
+            });
+        }
+    }
+}
