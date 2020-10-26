712a2ddc003bd5808b7279afe103ffda81143947
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-10-26 14:37:20 +0100

    RHR: Parse some more layer info, move level files to folder

--- Assets/Scripts/DataTypes/GBAIsometric/Level/GBAIsometric_LevelDataLayer.cs
+++ Assets/Scripts/DataTypes/GBAIsometric/Level/GBAIsometric_LevelDataLayer.cs
@@ -2,12 +2,12 @@
 {
     public class GBAIsometric_LevelDataLayer : R1Serializable
     {
-        public Pointer DataPointer { get; set; }
+        public Pointer<GBAIsometric_LevelDataLayerData> DataPointer { get; set; }
         public byte[] UnkData { get; set; }
 
         public override void SerializeImpl(SerializerObject s)
         {
-            DataPointer = s.SerializePointer(DataPointer, name: nameof(DataPointer));
+            DataPointer = s.SerializePointer<GBAIsometric_LevelDataLayerData>(DataPointer, resolve: true, name: nameof(DataPointer));
             UnkData = s.SerializeArray<byte>(UnkData, 16, name: nameof(UnkData));
         }
     }
