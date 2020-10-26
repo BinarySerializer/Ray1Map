55317cd3b3871ab337f0a64b98f791030d9a755d
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-10-26 20:58:47 +0100

    RHR: Add sprite class, add obj type function pointers

--- Assets/Scripts/DataTypes/GBAIsometric/Object/GBAIsometric_ObjectTypeData.cs
+++ Assets/Scripts/DataTypes/GBAIsometric/Object/GBAIsometric_ObjectTypeData.cs
@@ -4,11 +4,25 @@
     {
         public Pointer<GBAIsometric_AnimSet> AnimSetPointer { get; set; }
 
-        // TODO: More data - function pointers?
+        public Pointer InitFunctionPointer { get; set; }
+        public Pointer FunctionPointer1 { get; set; }
+        public Pointer FunctionPointer2 { get; set; } // For Lums this handles you collecting them
+        public Pointer FunctionPointer3 { get; set; }
+        public Pointer FunctionPointer4 { get; set; }
+
+        public byte[] UnkData { get; set; }
 
         public override void SerializeImpl(SerializerObject s)
         {
             AnimSetPointer = s.SerializePointer<GBAIsometric_AnimSet>(AnimSetPointer, resolve: true, name: nameof(AnimSetPointer));
+
+            InitFunctionPointer = s.SerializePointer(InitFunctionPointer, name: nameof(InitFunctionPointer));
+            FunctionPointer1 = s.SerializePointer(FunctionPointer1, name: nameof(FunctionPointer1));
+            FunctionPointer2 = s.SerializePointer(FunctionPointer2, name: nameof(FunctionPointer2));
+            FunctionPointer3 = s.SerializePointer(FunctionPointer3, name: nameof(FunctionPointer3));
+            FunctionPointer4 = s.SerializePointer(FunctionPointer4, name: nameof(FunctionPointer4));
+
+            UnkData = s.SerializeArray<byte>(UnkData, 4, name: nameof(UnkData));
         }
     }
 }
