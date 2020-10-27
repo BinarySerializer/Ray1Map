7cd22a4dacdaa62a51343c7f33b653355b3f1128
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-10-27 16:10:06 +0100

    RHR: Common graphics data struct

--- Assets/Scripts/DataTypes/GBAIsometric/Graphics/GBAIsometric_AnimSet.cs
+++ Assets/Scripts/DataTypes/GBAIsometric/Graphics/GBAIsometric_AnimSet.cs
@@ -8,7 +8,7 @@
         public Pointer Pointer_18 { get; set; }
         public Pointer Pointer_1C { get; set; } // Img data?
         public Pointer Pointer_20 { get; set; }
-        public Pointer Pointer_24 { get; set; } // Leads to 12 bytes + 2 pointers
+        public Pointer<GBAIsometric_GraphicsData> GraphicsDataPointer { get; set; }
         public byte[] Bytes_28 { get; set; }
         public Pointer NamePointer { get; set; }
 
@@ -23,7 +23,7 @@
             Pointer_18 = s.SerializePointer(Pointer_18, name: nameof(Pointer_18));
             Pointer_1C = s.SerializePointer(Pointer_1C, name: nameof(Pointer_1C));
             Pointer_20 = s.SerializePointer(Pointer_20, name: nameof(Pointer_20));
-            Pointer_24 = s.SerializePointer(Pointer_24, name: nameof(Pointer_24));
+            GraphicsDataPointer = s.SerializePointer<GBAIsometric_GraphicsData>(GraphicsDataPointer, resolve: true, name: nameof(GraphicsDataPointer));
             Bytes_28 = s.SerializeArray<byte>(Bytes_28, 8, name: nameof(Bytes_28));
             NamePointer = s.SerializePointer(NamePointer, name: nameof(NamePointer));
 
