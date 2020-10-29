79ef65e9d4f43b8690c7bf40293875cf6ddabd4d
byvar <39142447+byvar@users.noreply.github.com>
2020-10-29 16:34:03 +0100

    Forgot order of SerializeBitValues

--- Assets/Scripts/DataTypes/GBAIsometric/Graphics/GBAIsometric_SpriteSet.cs
+++ Assets/Scripts/DataTypes/GBAIsometric/Graphics/GBAIsometric_SpriteSet.cs
@@ -22,8 +22,8 @@
         public override void SerializeImpl(SerializerObject s)
         {
             s.SerializeBitValues<byte>((bitFunc) => {
-                Width = bitFunc(Width, 4, name: nameof(Width));
                 Height = bitFunc(Height, 4, name: nameof(Height));
+                Width = bitFunc(Width, 4, name: nameof(Width));
             });
             Byte_01 = s.Serialize<byte>(Byte_01, name: nameof(Byte_01));
             Byte_02 = s.Serialize<byte>(Byte_02, name: nameof(Byte_02));
