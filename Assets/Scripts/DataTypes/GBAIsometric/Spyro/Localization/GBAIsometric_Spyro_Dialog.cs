f74c4633446726a43c06741b087514d5e19e2c9a
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-11-09 18:37:27 +0100

    Spyro: Refactor dialog code to work with Spyro 2

--- Assets/Scripts/DataTypes/GBAIsometric/Spyro/Localization/GBAIsometric_Spyro_Dialog.cs
+++ Assets/Scripts/DataTypes/GBAIsometric/Spyro/Localization/GBAIsometric_Spyro_Dialog.cs
@@ -12,8 +12,7 @@
             ID = s.Serialize<uint>(ID, name: nameof(ID));
             DialogDataPointer = s.SerializePointer(DialogDataPointer, name: nameof(DialogDataPointer));
 
-            if (s.GameSettings.EngineVersion == EngineVersion.GBAIsometric_Spyro3)
-                DialogData = s.DoAt(DialogDataPointer, () => s.SerializeObject<GBAIsometric_Spyro_DialogData>(DialogData, name: nameof(DialogData)));
+            DialogData = s.DoAt(DialogDataPointer, () => s.SerializeObject<GBAIsometric_Spyro_DialogData>(DialogData, name: nameof(DialogData)));
         }
     }
 }
