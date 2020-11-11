b6cf83c3cff91dfe49aac6c390b7a06cf40c7d03
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-11-11 19:14:56 +0100

    R1 PC: Fill out credits string struct

--- Assets/Scripts/DataTypes/Rayman1/PC/General/R1_PC_GeneralFileStringItem.cs
+++ Assets/Scripts/DataTypes/Rayman1/PC/General/R1_PC_GeneralFileStringItem.cs
@@ -2,7 +2,12 @@
 {
     public class R1_PC_GeneralFileStringItem : R1Serializable
     {
-        public byte[] Unk1 { get; set; }
+        public uint StringPointer { get; set; } // Gets overwritten by a pointer in memory
+        public uint Uint_04 { get; set; }
+        public ushort TextNum { get; set; }
+        public byte FontSize { get; set; } // 1 = force uppercase, 2 = normal
+        public byte Byte_0B { get; set; }
+        public uint TextColor { get; set; } // 0-5
 
         public R1_PC_LocFileString String { get; set; }
 
@@ -12,7 +17,12 @@
         /// <param name="s">The serializer object</param>
         public override void SerializeImpl(SerializerObject s)
         {
-            Unk1 = s.SerializeArray<byte>(Unk1, 16, name: nameof(Unk1));
+            StringPointer = s.Serialize<uint>(StringPointer, name: nameof(StringPointer));
+            Uint_04 = s.Serialize<uint>(Uint_04, name: nameof(Uint_04));
+            TextNum = s.Serialize<ushort>(TextNum, name: nameof(TextNum));
+            FontSize = s.Serialize<byte>(FontSize, name: nameof(FontSize));
+            Byte_0B = s.Serialize<byte>(Byte_0B, name: nameof(Byte_0B));
+            TextColor = s.Serialize<uint>(TextColor, name: nameof(TextColor));
             String = s.SerializeObject<R1_PC_LocFileString>(String, name: nameof(String));
         }
     }
