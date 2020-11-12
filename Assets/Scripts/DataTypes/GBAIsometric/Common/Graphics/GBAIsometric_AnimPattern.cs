862d5b72a2fed1d62f57e3f1f26538fb7d77bc7f
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-11-12 09:26:13 +0100

    Spyro: Attempt to parse some anim pattern values

--- Assets/Scripts/DataTypes/GBAIsometric/Common/Graphics/GBAIsometric_AnimPattern.cs
+++ Assets/Scripts/DataTypes/GBAIsometric/Common/Graphics/GBAIsometric_AnimPattern.cs
@@ -13,7 +13,12 @@
         public int Width => 1 << WidthPower;
         public int Height => 1 << HeightPower;
 
-        public byte[] Spyro_Bytes { get; set; }
+        public ushort Spyro_TileIndex { get; set; }
+        public byte Spyro_Width { get; set; } // In pixels
+        public byte Spyro_Height { get; set; }
+        public byte[] Spyro_Bytes_04 { get; set; }
+        public byte Spyro_NumTiles { get; set; }
+        public byte[] Spyro_Bytes_09 { get; set; }
 
         public override void SerializeImpl(SerializerObject s)
         {
@@ -30,7 +35,12 @@
             }
             else
             {
-                Spyro_Bytes = s.SerializeArray<byte>(Spyro_Bytes, 16, name: nameof(Spyro_Bytes));
+                Spyro_TileIndex = s.Serialize<ushort>(Spyro_TileIndex, name: nameof(Spyro_TileIndex));
+                Spyro_Width = s.Serialize<byte>(Spyro_Width, name: nameof(Spyro_Width));
+                Spyro_Height = s.Serialize<byte>(Spyro_Height, name: nameof(Spyro_Height));
+                Spyro_Bytes_04 = s.SerializeArray<byte>(Spyro_Bytes_04, 4, name: nameof(Spyro_Bytes_04));
+                Spyro_NumTiles = s.Serialize<byte>(Spyro_NumTiles, name: nameof(Spyro_NumTiles));
+                Spyro_Bytes_09 = s.SerializeArray<byte>(Spyro_Bytes_09, 7, name: nameof(Spyro_Bytes_09));
             }
         }
     }
