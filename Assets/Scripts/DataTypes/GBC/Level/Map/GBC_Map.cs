448f8ad330395691102b00ce225e9c8e75e9a887
byvar <39142447+byvar@users.noreply.github.com>
2020-11-25 22:24:46 +0100

    GBC: Parse main map block

--- Assets/Scripts/DataTypes/GBC/Level/Map/GBC_Map.cs
+++ Assets/Scripts/DataTypes/GBC/Level/Map/GBC_Map.cs
@@ -6,12 +6,19 @@
         public uint Height { get; set; }
         public MapTile[] MapTiles { get; set; }
 
+        // GBC
+        public byte PaletteCount { get; set; }
+        public byte UnkStructCount1 { get; set; }
+        public byte UnkStructCount2 { get; set; }
+        public GBC_MapEntry[] UnkStructs1 { get; set; }
+        public GBC_MapEntry[] UnkStructs2 { get; set; }
+        public ARGB1555Color[] Palette { get; set; }
+
         // Parsed from offset table
         public GBC_TileKit TileKit { get; set; }
         public GBC_MapBlock Block1 { get; set; }
         public GBC_MapBlock Block2 { get; set; }
         public GBC_MapBlock Collision { get; set; }
-
 
         public override void SerializeImpl(SerializerObject s)
         {
@@ -21,7 +28,12 @@
             if (s.GameSettings.EngineVersion == EngineVersion.GBC_R1) {
                 Width = s.Serialize<byte>((byte)Width, name: nameof(Width));
                 Height = s.Serialize<byte>((byte)Height, name: nameof(Height));
-                // TODO: Parse rest of data: 3 bytes, then 5-byte structs, then palette at the end
+                PaletteCount = s.Serialize<byte>(PaletteCount, name: nameof(PaletteCount));
+                UnkStructCount1 = s.Serialize<byte>(UnkStructCount1, name: nameof(UnkStructCount1));
+                UnkStructCount2 = s.Serialize<byte>(UnkStructCount2, name: nameof(UnkStructCount2));
+                UnkStructs1 = s.SerializeObjectArray<GBC_MapEntry>(UnkStructs1, UnkStructCount1, name: nameof(UnkStructs1));
+                UnkStructs2 = s.SerializeObjectArray<GBC_MapEntry>(UnkStructs2, UnkStructCount2, name: nameof(UnkStructs2));
+                Palette = s.SerializeObjectArray<ARGB1555Color>(Palette, PaletteCount * 4, name: nameof(Palette));
 
 
                 Block1 = s.DoAt(OffsetTable.GetPointer(1), () => s.SerializeObject<GBC_MapBlock>(Block1, onPreSerialize: b => {
---
+++ Assets/Scripts/DataTypes/GBC/Level/Map/GBC_MapEntry.cs
@@ -0,0 +1,18 @@
+﻿namespace R1Engine
+{
+    public class GBC_MapEntry : R1Serializable {
+        public byte Byte_00 { get; set; }
+        public ushort TileOffset { get; set; } // TileIndex = TileOffset / 16
+        public GBC_Pointer GBCPointer { get; set; } // TODO: Is this a ushort bit value or a pointer?
+        //public ushort UShort_03 { get; set; }
+
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            Byte_00 = s.Serialize<byte>(Byte_00, name: nameof(Byte_00));
+            TileOffset = s.Serialize<ushort>(TileOffset, name: nameof(TileOffset));
+            GBCPointer = s.SerializeObject<GBC_Pointer>(GBCPointer, onPreSerialize: p => p.HasMemoryBankValue = false, name: nameof(GBCPointer));
+            //UShort_03 = s.Serialize<ushort>(UShort_03, name: nameof(UShort_03));
+        }
+    }
+}
