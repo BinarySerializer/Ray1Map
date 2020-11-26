5b3c1acc8428236174f21c59951dc2a8073474b9
byvar <39142447+byvar@users.noreply.github.com>
2020-11-26 20:28:07 +0100

    GBC: One extra parsed byte in Scene, yay

--- Assets/Scripts/DataTypes/GBC/Level/GBC_Scene.cs
+++ Assets/Scripts/DataTypes/GBC/Level/GBC_Scene.cs
@@ -2,6 +2,7 @@
 {
     public class GBC_Scene : GBC_BaseBlock
     {
+        public byte SceneIndex { get; set; }
         public byte[] UnkData0 { get; set; }
         public byte ExitsCount { get; set; }
         public byte[] UnkData1 { get; set; } // This data is identical across GBC and PalmOS, so most likely no 16/32-bit values
@@ -10,10 +11,10 @@
         // Parsed
         public GBC_PlayField PlayField { get; set; }
 
-        public override void SerializeBlock(SerializerObject s)
-        {
+        public override void SerializeBlock(SerializerObject s) {
             // TODO: Parse data
-            UnkData0 = s.SerializeArray<byte>(UnkData0, 4, name: nameof(UnkData0));
+            SceneIndex = s.Serialize<byte>(SceneIndex, name: nameof(SceneIndex));
+            UnkData0 = s.SerializeArray<byte>(UnkData0, 3, name: nameof(UnkData0));
             ExitsCount = s.Serialize<byte>(ExitsCount, name: nameof(ExitsCount));
             UnkData1 = s.SerializeArray<byte>(UnkData1, 61, name: nameof(UnkData0));
             Exits = s.SerializeArray<byte>(Exits, ExitsCount, name: nameof(Exits));
