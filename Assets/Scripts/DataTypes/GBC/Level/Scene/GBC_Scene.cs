4cb55043822ca5bdb78e65ef290298c810a8c185
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-11-29 11:45:25 +0100

    GBC: Unknown palette is the tile palette

--- Assets/Scripts/DataTypes/GBC/Level/Scene/GBC_Scene.cs
+++ Assets/Scripts/DataTypes/GBC/Level/Scene/GBC_Scene.cs
@@ -17,7 +17,7 @@ namespace R1Engine
         public byte IndexMax_ActorModels { get; set; }
 
         public ARGB1555Color[] ObjPalette { get; set; }
-        public ARGB1555Color[] UnknownPalette { get; set; }
+        public ARGB1555Color[] TilePalette { get; set; }
         public ushort MainActor_0 { get; set; }
         public ushort MainActor_1 { get; set; }
         public ushort MainActor_2 { get; set; }
@@ -54,7 +54,7 @@ namespace R1Engine
                 IndexMin_ActorModels = s.Serialize<byte>(IndexMin_ActorModels, name: nameof(IndexMin_ActorModels));
                 IndexMax_ActorModels = s.Serialize<byte>(IndexMax_ActorModels, name: nameof(IndexMax_ActorModels));
                 ObjPalette = s.SerializeObjectArray<ARGB1555Color>(ObjPalette, 8 * 4, name: nameof(ObjPalette));
-                UnknownPalette = s.SerializeObjectArray<ARGB1555Color>(UnknownPalette, 8 * 4, name: nameof(UnknownPalette));
+                TilePalette = s.SerializeObjectArray<ARGB1555Color>(TilePalette, 8 * 4, name: nameof(TilePalette));
                 MainActor_0 = s.Serialize<ushort>(MainActor_0, name: nameof(MainActor_0));
                 MainActor_1 = s.Serialize<ushort>(MainActor_1, name: nameof(MainActor_1));
                 MainActor_2 = s.Serialize<ushort>(MainActor_2, name: nameof(MainActor_2));
