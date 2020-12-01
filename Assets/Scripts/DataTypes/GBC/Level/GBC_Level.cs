bbd0f9673b1cf1e730f7db65f520473605a6ec69
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-12-01 21:24:20 +0100

    GBC: Level index in level struct

--- Assets/Scripts/DataTypes/GBC/Level/GBC_Level.cs
+++ Assets/Scripts/DataTypes/GBC/Level/GBC_Level.cs
@@ -3,9 +3,10 @@
     public class GBC_Level : GBC_BaseBlock
     {
         public byte LevelIndex { get; set; }
+        public byte InGameLevelIndex { get; set; } // The order of the levels in-game
         public byte[] UnkData0 { get; set; }
         public byte LinkedLevelsCount { get; set; }
-        public byte[] UnkData1 { get; set; }
+        public byte[] UnkData1 { get; set; } // Second byte seems to determine the cutscene types before the level starts?
         public byte[] LinkedLevels { get; set; } // exit block index = exit + 1
         
         // Parsed
@@ -13,9 +14,10 @@
 
         public override void SerializeBlock(SerializerObject s) {
             LevelIndex = s.Serialize<byte>(LevelIndex, name: nameof(LevelIndex));
-            UnkData0 = s.SerializeArray<byte>(UnkData0, 3, name: nameof(UnkData0));
+            InGameLevelIndex = s.Serialize<byte>(InGameLevelIndex, name: nameof(InGameLevelIndex));
+            UnkData0 = s.SerializeArray<byte>(UnkData0, 2, name: nameof(UnkData0));
             LinkedLevelsCount = s.Serialize<byte>(LinkedLevelsCount, name: nameof(LinkedLevelsCount));
-            UnkData1 = s.SerializeArray<byte>(UnkData1, 61, name: nameof(UnkData0));
+            UnkData1 = s.SerializeArray<byte>(UnkData1, 61, name: nameof(UnkData1));
             LinkedLevels = s.SerializeArray<byte>(LinkedLevels, LinkedLevelsCount, name: nameof(LinkedLevels));
 
             // TODO: Parse remaining data
