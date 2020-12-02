22bc7fd8b01d11ad0aa0eea0239d6106745617c4
byvar <39142447+byvar@users.noreply.github.com>
2020-12-02 18:27:08 +0100

    GBC: More parsed Level values

--- Assets/Scripts/DataTypes/GBC/Level/GBC_Level.cs
+++ Assets/Scripts/DataTypes/GBC/Level/GBC_Level.cs
@@ -4,10 +4,13 @@
     {
         public byte LevelIndex { get; set; }
         public sbyte InGameLevelIndex { get; set; } // The order of the levels in-game (-1 for optional levels)
-        public byte[] UnkData0 { get; set; }
+        public byte Byte_02 { get; set; }
+        public MapType Type { get; set; }
         public byte LinkedLevelsCount { get; set; }
         public bool IsGift { get; set; }
         public sbyte LinkedLevelsStartIndex { get; set; } // Actually start index + 1 (-1 if there are no linked levels)
+        public sbyte TimeOut { get; set; }
+        public sbyte NbTings { get; set; }
         public byte[] UnkData1 { get; set; }
         public byte[] LinkedLevels { get; set; } // level block index = LinkedLevelsStartIndex - 1 + LinkedLevels[i]
         
@@ -17,17 +20,26 @@
         public override void SerializeBlock(SerializerObject s) {
             LevelIndex = s.Serialize<byte>(LevelIndex, name: nameof(LevelIndex));
             InGameLevelIndex = s.Serialize<sbyte>(InGameLevelIndex, name: nameof(InGameLevelIndex));
-            UnkData0 = s.SerializeArray<byte>(UnkData0, 2, name: nameof(UnkData0));
+            Byte_02 = s.Serialize<byte>(Byte_02, name: nameof(Byte_02));
+            Type = s.Serialize<MapType>(Type, name: nameof(Type));
             LinkedLevelsCount = s.Serialize<byte>(LinkedLevelsCount, name: nameof(LinkedLevelsCount));
             IsGift = s.Serialize<bool>(IsGift, name: nameof(IsGift));
             LinkedLevelsStartIndex = s.Serialize<sbyte>(LinkedLevelsStartIndex, name: nameof(LinkedLevelsStartIndex));
-            UnkData1 = s.SerializeArray<byte>(UnkData1, 59, name: nameof(UnkData1));
+            TimeOut = s.Serialize<sbyte>(TimeOut, name: nameof(TimeOut));
+            NbTings = s.Serialize<sbyte>(NbTings, name: nameof(NbTings));
+            UnkData1 = s.SerializeArray<byte>(UnkData1, 57, name: nameof(UnkData1));
             LinkedLevels = s.SerializeArray<byte>(LinkedLevels, LinkedLevelsCount, name: nameof(LinkedLevels));
 
             // TODO: Parse remaining data
 
             // Parse data from pointers
             Scene = s.DoAt(DependencyTable.GetPointer(0), () => s.SerializeObject<GBC_Scene>(Scene, name: nameof(Scene)));
+        }
+
+        public enum MapType : byte {
+            Normal = 0,
+            Bonus = 1,
+            WorldMap = 2
         }
     }
 }
