6ab9139e4c45fed476a87707411ab6f20616282c
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-12-02 09:23:37 +0100

    GBC: Minor fixes to level struct

--- Assets/Scripts/DataTypes/GBC/Level/GBC_Level.cs
+++ Assets/Scripts/DataTypes/GBC/Level/GBC_Level.cs
@@ -3,12 +3,12 @@
     public class GBC_Level : GBC_BaseBlock
     {
         public byte LevelIndex { get; set; }
-        public byte InGameLevelIndex { get; set; } // The order of the levels in-game
+        public sbyte InGameLevelIndex { get; set; } // The order of the levels in-game (-1 for optional levels)
         public byte[] UnkData0 { get; set; }
         public byte LinkedLevelsCount { get; set; }
-        public byte IsGift { get; set; }
-        public byte LinkedLevelsStartIndex { get; set; } // Actually start index + 1
-        public byte[] UnkData1 { get; set; } // Second byte seems to determine the cutscene types before the level starts?
+        public bool IsGift { get; set; }
+        public sbyte LinkedLevelsStartIndex { get; set; } // Actually start index + 1 (-1 if there are no linked levels)
+        public byte[] UnkData1 { get; set; }
         public byte[] LinkedLevels { get; set; } // level block index = LinkedLevelsStartIndex - 1 + LinkedLevels[i]
         
         // Parsed
@@ -16,11 +16,11 @@
 
         public override void SerializeBlock(SerializerObject s) {
             LevelIndex = s.Serialize<byte>(LevelIndex, name: nameof(LevelIndex));
-            InGameLevelIndex = s.Serialize<byte>(InGameLevelIndex, name: nameof(InGameLevelIndex));
+            InGameLevelIndex = s.Serialize<sbyte>(InGameLevelIndex, name: nameof(InGameLevelIndex));
             UnkData0 = s.SerializeArray<byte>(UnkData0, 2, name: nameof(UnkData0));
             LinkedLevelsCount = s.Serialize<byte>(LinkedLevelsCount, name: nameof(LinkedLevelsCount));
-            IsGift = s.Serialize<byte>(IsGift, name: nameof(IsGift));
-            LinkedLevelsStartIndex = s.Serialize<byte>(LinkedLevelsStartIndex, name: nameof(LinkedLevelsStartIndex));
+            IsGift = s.Serialize<bool>(IsGift, name: nameof(IsGift));
+            LinkedLevelsStartIndex = s.Serialize<sbyte>(LinkedLevelsStartIndex, name: nameof(LinkedLevelsStartIndex));
             UnkData1 = s.SerializeArray<byte>(UnkData1, 59, name: nameof(UnkData1));
             LinkedLevels = s.SerializeArray<byte>(LinkedLevels, LinkedLevelsCount, name: nameof(LinkedLevels));
 
