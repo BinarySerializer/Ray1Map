9d138b3c47a36b280600106341c0af6c70aa9597
byvar <39142447+byvar@users.noreply.github.com>
2020-12-12 07:19:52 +0100

    SNES proto: Serialize image descriptors

---
+++ Assets/Scripts/DataTypes/SNES/SNES_Proto_ImageDescriptor.cs
@@ -0,0 +1,29 @@
+﻿namespace R1Engine
+{
+    public class SNES_Proto_ImageDescriptor : R1Serializable
+    {
+        public bool IsLarge { get; set; }
+        public byte Unknown { get; set; }
+        public int TileIndex { get; set; }
+        public int Palette { get; set; }
+        public int Priority { get; set; }
+        public bool FlipX { get; set; }
+        public bool FlipY { get; set; }
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            s.SerializeBitValues<byte>(bitFunc => {
+                Unknown = (byte)bitFunc(Unknown, 7, name: nameof(Unknown));
+                IsLarge = bitFunc(IsLarge ? 1 : 0, 1, name: nameof(IsLarge)) == 1;
+            });
+            s.SerializeBitValues<ushort>(bitFunc =>
+            {
+                TileIndex = (ushort)bitFunc(TileIndex, 9, name: nameof(TileIndex));
+                Palette = bitFunc(Palette, 3, name: nameof(Palette));
+                Priority = bitFunc(Priority, 2, name: nameof(Priority));
+                FlipX = bitFunc(FlipX ? 1 : 0, 1, name: nameof(FlipX)) == 1;
+                FlipY = bitFunc(FlipY ? 1 : 0, 1, name: nameof(FlipY)) == 1;
+            });
+        }
+    }
+}
--- Assets/Scripts/DataTypes/SNES/SNES_Proto_ROM.cs
+++ Assets/Scripts/DataTypes/SNES/SNES_Proto_ROM.cs
@@ -1,4 +1,6 @@
-﻿namespace R1Engine
+﻿using System.Linq;
+
+namespace R1Engine
 {
     public class SNES_Proto_ROM : R1Serializable
     {
@@ -6,7 +8,9 @@
         public RGBA5551Color[] Palettes { get; set; }
         public SNES_Proto_TileDescriptor[] TileDescriptors { get; set; }
         public byte[] TileMap { get; set; }
+
         public SNES_Proto_State[] States { get; set; }
+        public SNES_Proto_ImageDescriptor[] ImageDescriptors { get; set; }
 
         /// <summary>
         /// Handles the data serialization
@@ -18,7 +22,10 @@
             Palettes = s.DoAt(s.CurrentPointer + 0x2ADC4, () => s.SerializeObjectArray<RGBA5551Color>(Palettes, 16* 16, name: nameof(Palettes)));
             TileDescriptors = s.DoAt(s.CurrentPointer + 0x1AAF8, () => s.SerializeObjectArray<SNES_Proto_TileDescriptor>(TileDescriptors, 1024 * 4, name: nameof(TileDescriptors)));
             TileMap = s.DoAt(s.CurrentPointer + 0x30000, () => s.SerializeArray<byte>(TileMap, 0x10000, name: nameof(TileMap)));
+            
+            
             States = s.DoAt(s.CurrentPointer + 0x210AC, () => s.SerializeObjectArray<SNES_Proto_State>(States, 5 * 0x15, name: nameof(States)));
+            ImageDescriptors = s.DoAt(s.CurrentPointer + 0x20f4c, () => s.SerializeObjectArray<SNES_Proto_ImageDescriptor>(ImageDescriptors, States.Max(state => state.Animation?.Layers.Max(layer => layer.ImageIndex + 1) ?? 0), name: nameof(ImageDescriptors)));
         }
     }
 }
