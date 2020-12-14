a502853d866b3803229dc11e28f2181803276ad3
byvar <39142447+byvar@users.noreply.github.com>
2020-12-14 06:55:37 +0100

    SNES: Correct animated tiles struct

--- Assets/Scripts/DataTypes/SNES/SNES_Proto_AnimatedTileEntry.cs
+++ Assets/Scripts/DataTypes/SNES/SNES_Proto_AnimatedTileEntry.cs
@@ -4,8 +4,7 @@
     {
         public SNES_Pointer GraphicsPointer { get; set; }
         public ushort VRAMAddress { get; set; }
-        public byte Byte_05 { get; set; }
-        public byte UnknownCount { get; set; }
+        public ushort BytesToCopy { get; set; }
         public byte[] Unknown { get; set; }
 
         public byte[] GraphicsBuffer { get; set; }
@@ -14,12 +13,11 @@
         {
             GraphicsPointer = s.SerializeObject<SNES_Pointer>(GraphicsPointer, onPreSerialize: p => p.HasMemoryBankValue = true, name: nameof(GraphicsPointer));
             VRAMAddress = s.Serialize<ushort>(VRAMAddress, name: nameof(VRAMAddress));
-            Byte_05 = s.Serialize<byte>(Byte_05, name: nameof(Byte_05));
-            UnknownCount = s.Serialize<byte>(UnknownCount, name: nameof(UnknownCount));
-            Unknown = s.SerializeArray<byte>(Unknown, UnknownCount, name: nameof(Unknown));
+            BytesToCopy = s.Serialize<ushort>(BytesToCopy, name: nameof(BytesToCopy));
+            Unknown = s.SerializeArray<byte>(Unknown, BytesToCopy >> 8, name: nameof(Unknown));
 
             GraphicsBuffer = s.DoAt(GraphicsPointer.GetPointer(), () => {
-                return s.SerializeArray<byte>(GraphicsBuffer, 0x20 * (Byte_05 == 0x60 ? 19 : 32), name: nameof(GraphicsBuffer));
+                return s.SerializeArray<byte>(GraphicsBuffer, BytesToCopy, name: nameof(GraphicsBuffer));
             });
         }
     }
