9ddd96d1aacce384c0cc1a6f93deca172ce12ccd
byvar <39142447+byvar@users.noreply.github.com>
2020-12-14 23:37:54 +0100

    SNES: Edit custom animations for enemy

--- Assets/Scripts/Managers/SNES/SNES_Prototype_Manager.cs
+++ Assets/Scripts/Managers/SNES/SNES_Prototype_Manager.cs
@@ -339,7 +339,7 @@ namespace R1Engine
                 GetGraphicsGroup(rom, CustomAnimations.Select(x => new SNES_Proto_State()
                 {
                     Animation = x,
-                    Flags = SNES_Proto_State.StateFlags.None,
+                    Flags = SNES_Proto_State.StateFlags.UseCurrentFlip,
                     AnimSpeed = 8 // TODO: Allow each animation to have a different speed
                 }).ToArray(), CustomImageDescriptors, true, "Unused (recreated)"),
             };
@@ -589,42 +589,42 @@ namespace R1Engine
                 FrameCount = 8, // 4 frames, pingponged
                 Layers = new R1_AnimationLayer[][] {
                     new R1_AnimationLayer[] { // Frame 0
-                        new R1_AnimationLayer() { XPosition = 12, YPosition = 4, ImageIndex = 16 },
+                        new R1_AnimationLayer() { XPosition = 11, YPosition = 4, ImageIndex = 2 }, // Back
                         new R1_AnimationLayer() { XPosition = 0, YPosition = 4, ImageIndex = 1 },
                         new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 2 },
                     },
                     new R1_AnimationLayer[] { // Frame 1
-                        new R1_AnimationLayer() { XPosition = 11, YPosition = 3, ImageIndex = 16 },
+                        new R1_AnimationLayer() { XPosition = 10, YPosition = 3, ImageIndex = 2 },
                         new R1_AnimationLayer() { XPosition = 0, YPosition = 4, ImageIndex = 3 },
                         new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 4 },
                     },
                     new R1_AnimationLayer[] { // Frame 2
-                        new R1_AnimationLayer() { XPosition = 10, YPosition = 3, ImageIndex = 16 },
+                        new R1_AnimationLayer() { XPosition = 9, YPosition = 3, ImageIndex = 2 },
                         new R1_AnimationLayer() { XPosition = 0, YPosition = 4, ImageIndex = 5 },
                         new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 6 },
                     },
                     new R1_AnimationLayer[] { // Frame 3
-                        new R1_AnimationLayer() { XPosition = 8, YPosition = 3, ImageIndex = 16 },
+                        new R1_AnimationLayer() { XPosition = 8, YPosition = 3, ImageIndex = 2 },
                         new R1_AnimationLayer() { XPosition = 0, YPosition = 4, ImageIndex = 7 },
                         new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 8 },
                     },
                     new R1_AnimationLayer[] { // Frame 3
-                        new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 16 },
+                        new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 2 },
                         new R1_AnimationLayer() { XPosition = 0, YPosition = 4, ImageIndex = 7 },
                         new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 8 },
                     },
                     new R1_AnimationLayer[] { // Frame 2
-                        new R1_AnimationLayer() { XPosition = 10, YPosition = 4, ImageIndex = 16 },
+                        new R1_AnimationLayer() { XPosition = 9, YPosition = 4, ImageIndex = 2 },
                         new R1_AnimationLayer() { XPosition = 0, YPosition = 3, ImageIndex = 5 },
                         new R1_AnimationLayer() { XPosition = 8, YPosition = 3, ImageIndex = 6 },
                     },
                     new R1_AnimationLayer[] { // Frame 1
-                        new R1_AnimationLayer() { XPosition = 11, YPosition = 4, ImageIndex = 16 },
+                        new R1_AnimationLayer() { XPosition = 10, YPosition = 4, ImageIndex = 2 },
                         new R1_AnimationLayer() { XPosition = 0, YPosition = 3, ImageIndex = 3 },
                         new R1_AnimationLayer() { XPosition = 8, YPosition = 3, ImageIndex = 4 },
                     },
                     new R1_AnimationLayer[] { // Frame 0
-                        new R1_AnimationLayer() { XPosition = 12, YPosition = 4, ImageIndex = 16 },
+                        new R1_AnimationLayer() { XPosition = 11, YPosition = 4, ImageIndex = 2 },
                         new R1_AnimationLayer() { XPosition = 0, YPosition = 3, ImageIndex = 1 },
                         new R1_AnimationLayer() { XPosition = 8, YPosition = 3, ImageIndex = 2 },
                     },
@@ -633,66 +633,133 @@ namespace R1Engine
             // Enemy surprised / stinger
             new R1Jaguar_AnimationDescriptor() {
                 LayersPerFrame = 4,
-                FrameCount = 6,
+                FrameCount = 9,
                 Layers = new R1_AnimationLayer[][] {
                     new R1_AnimationLayer[] { // Frame 0
-                        new R1_AnimationLayer() { XPosition = 12, YPosition = 3, ImageIndex = 16 }, // Back
-                        new R1_AnimationLayer() { XPosition = 16, YPosition = 3, ImageIndex = 15 }, // Stinger
-                        new R1_AnimationLayer() { XPosition = 0, YPosition = 2, ImageIndex = 13 }, // Body
-                        new R1_AnimationLayer() { XPosition = 8, YPosition = 2, ImageIndex = 14 },
+                        new R1_AnimationLayer() { XPosition = 0, YPosition = 4, ImageIndex = 1 }, // Body
+                        new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 2 },
+                        new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 0 }, // Back
+                        new R1_AnimationLayer() { XPosition = 12, YPosition = 4, ImageIndex = 0 }, // Stinger
+                    },
+                    new R1_AnimationLayer[] { // Frame 0
+                        new R1_AnimationLayer() { XPosition = 0, YPosition = 4, ImageIndex = 1 }, // Body
+                        new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 2 },
+                        new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 16 }, // Back
+                        new R1_AnimationLayer() { XPosition = 12, YPosition = 4, ImageIndex = 0 }, // Stinger
+                    },
+                    new R1_AnimationLayer[] { // Frame 5
+                        new R1_AnimationLayer() { XPosition = 9, YPosition = 4, ImageIndex = 16 }, // Back
+                        new R1_AnimationLayer() { XPosition = 14, YPosition = 4, ImageIndex = 0 }, // Stinger
+                        new R1_AnimationLayer() { XPosition = 0, YPosition = 4, ImageIndex = 5 }, // Body
+                        new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 6 },
                     },
                     new R1_AnimationLayer[] { // Frame 1
-                        new R1_AnimationLayer() { XPosition = 12, YPosition = 2, ImageIndex = 16 }, // Back
-                        new R1_AnimationLayer() { XPosition = 17, YPosition = 3, ImageIndex = 15 }, // Stinger
+                        new R1_AnimationLayer() { XPosition = 9, YPosition = 2, ImageIndex = 16 }, // Back
+                        new R1_AnimationLayer() { XPosition = 14, YPosition = 2, ImageIndex = 15 }, // Stinger
                         new R1_AnimationLayer() { XPosition = 0, YPosition = 1, ImageIndex = 13 }, // Body
                         new R1_AnimationLayer() { XPosition = 8, YPosition = 1, ImageIndex = 14 },
                     },
                     new R1_AnimationLayer[] { // Frame 2
-                        new R1_AnimationLayer() { XPosition = 12, YPosition = 2, ImageIndex = 16 }, // Back
-                        new R1_AnimationLayer() { XPosition = 18, YPosition = 2, ImageIndex = 15 }, // Stinger
+                        new R1_AnimationLayer() { XPosition = 9, YPosition = 2, ImageIndex = 16 }, // Back
+                        new R1_AnimationLayer() { XPosition = 15, YPosition = 2, ImageIndex = 15 }, // Stinger
                         new R1_AnimationLayer() { XPosition = 0, YPosition = 2, ImageIndex = 13 }, // Body
                         new R1_AnimationLayer() { XPosition = 8, YPosition = 2, ImageIndex = 14 },
                     },
                     new R1_AnimationLayer[] { // Frame 3
-                        new R1_AnimationLayer() { XPosition = 12, YPosition = 3, ImageIndex = 16 }, // Back
-                        new R1_AnimationLayer() { XPosition = 18, YPosition = 2, ImageIndex = 15 }, // Stinger
+                        new R1_AnimationLayer() { XPosition = 9, YPosition = 3, ImageIndex = 16 }, // Back
+                        new R1_AnimationLayer() { XPosition = 15, YPosition = 3, ImageIndex = 15 }, // Stinger
                         new R1_AnimationLayer() { XPosition = 0, YPosition = 4, ImageIndex = 13 }, // Body
                         new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 14 },
                     },
                     new R1_AnimationLayer[] { // Frame 4
-                        new R1_AnimationLayer() { XPosition = 12, YPosition = 4, ImageIndex = 16 }, // Back
-                        new R1_AnimationLayer() { XPosition = 17, YPosition = 2, ImageIndex = 15 }, // Stinger
+                        new R1_AnimationLayer() { XPosition = 9, YPosition = 4, ImageIndex = 16 }, // Back
+                        new R1_AnimationLayer() { XPosition = 14, YPosition = 4, ImageIndex = 15 }, // Stinger
                         new R1_AnimationLayer() { XPosition = 0, YPosition = 4, ImageIndex = 13 }, // Body
                         new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 14 },
                     },
                     new R1_AnimationLayer[] { // Frame 5
-                        new R1_AnimationLayer() { XPosition = 12, YPosition = 4, ImageIndex = 16 }, // Back
-                        new R1_AnimationLayer() { XPosition = 18, YPosition = 3, ImageIndex = 0 }, // Stinger
+                        new R1_AnimationLayer() { XPosition = 9, YPosition = 4, ImageIndex = 16 }, // Back
+                        new R1_AnimationLayer() { XPosition = 14, YPosition = 3, ImageIndex = 0 }, // Stinger
                         new R1_AnimationLayer() { XPosition = 0, YPosition = 4, ImageIndex = 5 }, // Body
                         new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 6 },
                     },
-                }.SelectMany(ls => ls).ToArray()
-            },
-            new R1Jaguar_AnimationDescriptor() {
-                LayersPerFrame = 2,
-                FrameCount = 1,
-                Layers = new R1_AnimationLayer[][] {
                     new R1_AnimationLayer[] { // Frame 0
-                        new R1_AnimationLayer() { XPosition = 0, YPosition = 0, ImageIndex = 9 }, // Body
-                        new R1_AnimationLayer() { XPosition = 0, YPosition = 8, ImageIndex = 10 },
+                        new R1_AnimationLayer() { XPosition = 0, YPosition = 4, ImageIndex = 1 }, // Body
+                        new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 2 },
+                        new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 0 }, // Back
+                        new R1_AnimationLayer() { XPosition = 12, YPosition = 4, ImageIndex = 0 }, // Stinger
                     },
                 }.SelectMany(ls => ls).ToArray()
             },
+            // Enemy vertical
             new R1Jaguar_AnimationDescriptor() {
-                LayersPerFrame = 2,
-                FrameCount = 1,
+                LayersPerFrame = 3,
+                FrameCount = 6,
                 Layers = new R1_AnimationLayer[][] {
                     new R1_AnimationLayer[] { // Frame 0
-                        new R1_AnimationLayer() { XPosition = 4, YPosition = 4, ImageIndex = 11 }, // Body
-                        new R1_AnimationLayer() { XPosition = 0, YPosition = 8, ImageIndex = 12 },
+                        new R1_AnimationLayer() { XPosition = 0, YPosition = 11, ImageIndex = 10 }, // Back
+                        new R1_AnimationLayer() { XPosition = 1, YPosition = 0, ImageIndex = 9 }, // Face
+                        new R1_AnimationLayer() { XPosition = 0, YPosition = 8, ImageIndex = 10 }, // Body
+                    },
+                    new R1_AnimationLayer[] { // Frame 1
+                        new R1_AnimationLayer() { XPosition = 0, YPosition = 10, ImageIndex = 10 }, // Back
+                        new R1_AnimationLayer() { XPosition = 0, YPosition = 0, ImageIndex = 9 }, // Face
+                        new R1_AnimationLayer() { XPosition = 0, YPosition = 8, ImageIndex = 10 }, // Body
+                    },
+                    new R1_AnimationLayer[] { // Frame 2
+                        new R1_AnimationLayer() { XPosition = 0, YPosition = 9, ImageIndex = 10 }, // Back
+                        new R1_AnimationLayer() { XPosition = 0, YPosition = 0, ImageIndex = 9 }, // Face
+                        new R1_AnimationLayer() { XPosition = 1, YPosition = 8, ImageIndex = 10 }, // Body
+                    },
+                    new R1_AnimationLayer[] { // Frame 3
+                        new R1_AnimationLayer() { XPosition = 0, YPosition = 8, ImageIndex = 10 }, // Back
+                        new R1_AnimationLayer() { XPosition = 0, YPosition = 0, ImageIndex = 9 }, // Face
+                        new R1_AnimationLayer() { XPosition = 1, YPosition = 7, ImageIndex = 10 }, // Body
+                    },
+                    new R1_AnimationLayer[] { // Frame 4
+                        new R1_AnimationLayer() { XPosition = 0, YPosition = 9, ImageIndex = 10 }, // Back
+                        new R1_AnimationLayer() { XPosition = 1, YPosition = 0, ImageIndex = 9 }, // Face
+                        new R1_AnimationLayer() { XPosition = 0, YPosition = 7, ImageIndex = 10 }, // Body
+                    },
+                    new R1_AnimationLayer[] { // Frame 5
+                        new R1_AnimationLayer() { XPosition = 0, YPosition = 10, ImageIndex = 10 }, // Back
+                        new R1_AnimationLayer() { XPosition = 1, YPosition = 0, ImageIndex = 9 }, // Face
+                        new R1_AnimationLayer() { XPosition = 0, YPosition = 8, ImageIndex = 10 }, // Body
                     },
                 }.SelectMany(ls => ls).ToArray()
             },
+            // Enemy vertical slope
+            new R1Jaguar_AnimationDescriptor() {
+                LayersPerFrame = 2,
+                FrameCount = 6,
+                Layers = new R1_AnimationLayer[][] {
+                    new R1_AnimationLayer[] { // Frame 0
+                        new R1_AnimationLayer() { XPosition = 4, YPosition = 8, ImageIndex = 12 }, // Back
+                        new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 11 }, // Face
+                    },
+                    new R1_AnimationLayer[] { // Frame 0
+                        new R1_AnimationLayer() { XPosition = 5, YPosition = 7, ImageIndex = 12 }, // Back
+                        new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 11 }, // Face
+                    },
+                    new R1_AnimationLayer[] { // Frame 0
+                        new R1_AnimationLayer() { XPosition = 6, YPosition = 6, ImageIndex = 12 }, // Back
+                        new R1_AnimationLayer() { XPosition = 9, YPosition = 3, ImageIndex = 11 }, // Face
+                    },
+                    new R1_AnimationLayer[] { // Frame 0
+                        new R1_AnimationLayer() { XPosition = 6, YPosition = 6, ImageIndex = 12 }, // Back
+                        new R1_AnimationLayer() { XPosition = 9, YPosition = 3, ImageIndex = 11 }, // Face
+                    },
+                    new R1_AnimationLayer[] { // Frame 0
+                        new R1_AnimationLayer() { XPosition = 5, YPosition = 7, ImageIndex = 12 }, // Back
+                        new R1_AnimationLayer() { XPosition = 9, YPosition = 3, ImageIndex = 11 }, // Face
+                    },
+                    new R1_AnimationLayer[] { // Frame 0
+                        new R1_AnimationLayer() { XPosition = 4, YPosition = 8, ImageIndex = 12 }, // Back
+                        new R1_AnimationLayer() { XPosition = 8, YPosition = 4, ImageIndex = 11 }, // Face
+                    },
+                }.SelectMany(ls => ls).ToArray()
+            },
+            
         };
     }
 }
