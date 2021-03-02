de415f24848bfb333fa73880466ede3e70fc08c7
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-03-02 09:21:45 +0100

    NitroKart: Parse and show background map layers

---
+++ Assets/Scripts/DataTypes/GBAVV/Level/NitroKart/GBAVV_NitroKart_BackgroundTileMap.cs
@@ -0,0 +1,12 @@
+﻿namespace R1Engine
+{
+    public class GBAVV_NitroKart_BackgroundTileMap : GBAVV_BaseBlock
+    {
+        public MapTile[] MapTiles { get; set; }
+
+        public override void SerializeBlock(SerializerObject s)
+        {
+            MapTiles = s.SerializeObjectArray<MapTile>(MapTiles, BlockLength / 2, name: nameof(MapTiles));
+        }
+    }
+}
