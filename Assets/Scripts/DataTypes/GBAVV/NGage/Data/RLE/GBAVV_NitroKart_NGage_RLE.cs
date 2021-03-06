8f9225c25557f662ae903078591f3d757b79d75d
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-03-06 15:21:08 +0100

    Nitro Kart NGage: Start parsing RLE data

---
+++ Assets/Scripts/DataTypes/GBAVV/NGage/Data/RLE/GBAVV_NitroKart_NGage_RLE.cs
@@ -0,0 +1,24 @@
+﻿namespace R1Engine
+{
+    public class GBAVV_NitroKart_NGage_RLE : R1Serializable
+    {
+        public string Magic { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int FramesCount { get; set; }
+        public Pointer[] FramePointers { get; set; }
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            Magic = s.SerializeString(Magic, 4, name: nameof(Magic));
+            Width = s.Serialize<int>(Width, name: nameof(Width));
+            Height = s.Serialize<int>(Height, name: nameof(Height));
+            FramesCount = s.Serialize<int>(FramesCount, name: nameof(FramesCount));
+            FramePointers = s.SerializePointerArray(FramePointers, FramesCount, name: nameof(FramePointers));
+
+            // TODO: Parse each frame
+
+            s.Goto(Offset + s.CurrentLength);
+        }
+    }
+}
--- Assets/Scripts/DataTypes/GBAVV/NGage/GBAVV_NitroKart_NGage_LevelInfo.cs
+++ Assets/Scripts/DataTypes/GBAVV/NGage/GBAVV_NitroKart_NGage_LevelInfo.cs
@@ -8,7 +8,7 @@
         public Pointer POPFilePathPointer { get; set; }
         public int Int_08 { get; set; }
         public Pointer ParallaxBaseFilePathPointer { get; set; }
-        public int Int_10 { get; set; }
+        public int ParallaxCount { get; set; }
         public int Int_14 { get; set; }
 
         // Serialized from pointers
@@ -19,7 +19,7 @@
         public GBAVV_NitroKart_NGage_PVS PVS { get; set; }
         public GBAVV_NitroKart_NGage_POP POP { get; set; }
         public GBAVV_NitroKart_NGage_PAL[] ParallaxPalettes { get; set; }
-        public byte[][] ParallaxData { get; set; } // TileSets? Animated like on GBA?
+        public GBAVV_NitroKart_NGage_RLE[] ParallaxData { get; set; }
 
         public override void SerializeImpl(SerializerObject s)
         {
@@ -27,7 +27,7 @@
             POPFilePathPointer = s.SerializePointer(POPFilePathPointer, name: nameof(POPFilePathPointer));
             Int_08 = s.Serialize<int>(Int_08, name: nameof(Int_08));
             ParallaxBaseFilePathPointer = s.SerializePointer(ParallaxBaseFilePathPointer, name: nameof(ParallaxBaseFilePathPointer));
-            Int_10 = s.Serialize<int>(Int_10, name: nameof(Int_10));
+            ParallaxCount = s.Serialize<int>(ParallaxCount, name: nameof(ParallaxCount));
             Int_14 = s.Serialize<int>(Int_14, name: nameof(Int_14));
 
             PVSFilePath = s.DoAt(PVSFilePathPointer, () => s.SerializeObject<GBAVV_NitroKart_NGage_FilePath>(PVSFilePath, name: nameof(PVSFilePath)));
@@ -41,13 +41,12 @@
             POP = POPFilePath.DoAtFile(() => s.SerializeObject<GBAVV_NitroKart_NGage_POP>(POP, name: nameof(POP)));
 
             if (ParallaxPalettes == null) ParallaxPalettes = new GBAVV_NitroKart_NGage_PAL[3];
-            if (ParallaxData == null) ParallaxData = new byte[3][];
+            if (ParallaxData == null) ParallaxData = new GBAVV_NitroKart_NGage_RLE[3];
 
-            // Every level has 3 background layers
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < ParallaxCount; i++)
             {
                 ParallaxPalettes[i] = ParallaxBaseFilePath.DoAtFile($"\\parallax{i}.pal", () => s.SerializeObject(ParallaxPalettes[i], name: $"{nameof(ParallaxPalettes)}[{i}]"));
-                ParallaxData[i] = ParallaxBaseFilePath.DoAtFile($"\\parallax{i}.rle", () => s.SerializeArray<byte>(ParallaxData[i], s.CurrentLength, name: $"{nameof(ParallaxData)}[{i}]"));
+                ParallaxData[i] = ParallaxBaseFilePath.DoAtFile($"\\parallax{i}.rle", () => s.SerializeObject<GBAVV_NitroKart_NGage_RLE>(ParallaxData[i], name: $"{nameof(ParallaxData)}[{i}]"));
             }
         }
     }
