bbadf0f496e3ba624090a8598e5d3b7453667f0e
byvar <39142447+byvar@users.noreply.github.com>
2021-03-31 01:00:19 +0200

    More work on GeometricObject

---
+++ Assets/Scripts/DataTypes/Jade/GEO/GEO_GeometricObject.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace R1Engine.Jade {
+	// Found in GEO_p_CreateFromBuffer
+	public class GEO_GeometricObject : GRO_GraphicRenderObject {
+		public uint Code_00 { get; set; }
+		public uint Type { get; set; }
+		public uint VerticesCount { get; set; }
+		public int HasMRM { get; set; }
+		public int HasShortPerVertex { get; set; }
+		public uint UInt_14_Count { get; set; }
+		public uint UVsCount { get; set; }
+		public uint ElementsCount { get; set; }
+		public uint UInt_Editor { get; set; }
+		public uint UInt_2C { get; set; }
+
+		public uint Code_01 { get; set; }
+		// (other stuff here)
+		public Jade_Vector[] Vertices { get; set; }
+		public Jade_Vector[] Vertices_Editor { get; set; }
+		public uint[] UInts_14 { get; set; }
+		public UV[] UVs { get; set; }
+		public GEO_GeometricObjectElement[] Elements { get; set; }
+
+		public override void SerializeImpl(SerializerObject s) {
+			LOA_Loader Loader = Context.GetStoredObject<LOA_Loader>(Jade_BaseManager.LoaderKey);
+
+			Code_00 = s.Serialize<uint>(Code_00, name: nameof(Code_00));
+			if (Code_00 == (uint)Jade_Code.Code2002) {
+				Type = s.Serialize<uint>(Type, name: nameof(Type));
+				VerticesCount = s.Serialize<uint>(VerticesCount, name: nameof(VerticesCount));
+				HasMRM = s.Serialize<int>(HasMRM, name: nameof(HasMRM));
+				if(HasMRM != 0) HasShortPerVertex = s.Serialize<int>(HasShortPerVertex, name: nameof(HasShortPerVertex));
+			} else {
+				VerticesCount = Code_00;
+				Type = 0;
+			}
+			UInt_14_Count = s.Serialize<uint>(UInt_14_Count, name: nameof(UInt_14_Count));
+			UVsCount = s.Serialize<uint>(UVsCount, name: nameof(UVsCount));
+			ElementsCount = s.Serialize<uint>(ElementsCount, name: nameof(ElementsCount));
+			if(!Loader.IsBinaryData) UInt_Editor = s.Serialize<uint>(UInt_Editor, name: nameof(UInt_Editor));
+			UInt_2C = s.Serialize<uint>(UInt_2C, name: nameof(UInt_2C));
+			Code_01 = s.Serialize<uint>(Code_01, name: nameof(Code_01));
+			if ((Code_01 & (uint)Jade_Code.Code2002) == (uint)Jade_Code.Code2002) {
+				throw new NotImplementedException($"TODO: Implement {GetType()}: SKN");
+			}
+			if ((Code_01 & 1) != 0) {
+				throw new NotImplementedException($"TODO: Implement {GetType()}: COL");
+			}
+			Vertices = s.SerializeObjectArray<Jade_Vector>(Vertices, VerticesCount, name: nameof(Vertices));
+			if (!Loader.IsBinaryData) Vertices_Editor = s.SerializeObjectArray<Jade_Vector>(Vertices_Editor, VerticesCount, name: nameof(Vertices_Editor));
+			if (UInt_2C != 0) {
+				throw new NotImplementedException($"TODO: Implement {GetType()}: UInt_2C");
+			}
+			UInts_14 = s.SerializeArray<uint>(UInts_14, UInt_14_Count, name: nameof(UInts_14));
+			UVs = s.SerializeObjectArray<UV>(UVs, UVsCount, name: nameof(UVs));
+			Elements = s.SerializeObjectArray<GEO_GeometricObjectElement>(Elements, ElementsCount, name: nameof(Elements));
+			foreach (var el in Elements) {
+				el.SerializeArrays(s);
+			}
+			throw new NotImplementedException($"TODO: Implement {GetType()}");
+		}
+
+		public class UV : R1Serializable {
+			public float U { get; set; }
+			public float V { get; set; }
+			public override void SerializeImpl(SerializerObject s) {
+				U = s.Serialize<float>(U, name: nameof(U));
+				V = s.Serialize<float>(V, name: nameof(V));
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/DataTypes/Jade/GEO/GEO_GeometricObjectElement.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace R1Engine.Jade {
+	// Found in GEO_v_CreateElementsFromBuffer
+	public class GEO_GeometricObjectElement : R1Serializable {
+		public uint TrianglesCount { get; set; }
+		public uint UInt_04 { get; set; }
+		public uint UInt_14 { get; set; }
+		public uint ShortsCount { get; set; }
+		public uint[] UInts_Editor { get; set; }
+
+		// Arrays
+		public Triangle[] Triangles { get; set; }
+		public UnknownStruct Unknown { get; set; }
+		public ushort[] Shorts { get; set; }
+
+		public override void SerializeImpl(SerializerObject s) {
+			LOA_Loader Loader = Context.GetStoredObject<LOA_Loader>(Jade_BaseManager.LoaderKey);
+
+			TrianglesCount = s.Serialize<uint>(TrianglesCount, name: nameof(TrianglesCount));
+			UInt_04 = s.Serialize<uint>(UInt_04, name: nameof(UInt_04));
+			UInt_14 = s.Serialize<uint>(UInt_14, name: nameof(UInt_14));
+			ShortsCount = s.Serialize<uint>(ShortsCount, name: nameof(ShortsCount));
+			if (!Loader.IsBinaryData) UInts_Editor = s.SerializeArray<uint>(UInts_Editor, 6, name: nameof(UInts_Editor));
+		}
+		public void SerializeArrays(SerializerObject s) {
+			Triangles = s.SerializeObjectArray<Triangle>(Triangles, TrianglesCount, name: nameof(Triangles));
+			if(UInt_14 != 0) Unknown = s.SerializeObject<UnknownStruct>(Unknown, name: nameof(Unknown));
+			Shorts = s.SerializeArray<ushort>(Shorts, ShortsCount, name: nameof(Shorts));
+		}
+
+		public class Triangle : R1Serializable {
+			public ushort Vertex0 { get; set; }
+			public ushort Vertex1 { get; set; }
+			public ushort Vertex2 { get; set; }
+			public ushort UV0 { get; set; }
+			public ushort UV1 { get; set; }
+			public ushort UV2 { get; set; }
+			public uint UInt_0C { get; set; }
+			public uint UInt_10 { get; set; }
+
+			public override void SerializeImpl(SerializerObject s) {
+				Vertex0 = s.Serialize<ushort>(Vertex0, name: nameof(Vertex0));
+				Vertex1 = s.Serialize<ushort>(Vertex1, name: nameof(Vertex1));
+				Vertex2 = s.Serialize<ushort>(Vertex2, name: nameof(Vertex2));
+				UV0 = s.Serialize<ushort>(UV0, name: nameof(UV0));
+				UV1 = s.Serialize<ushort>(UV1, name: nameof(UV1));
+				UV2 = s.Serialize<ushort>(UV2, name: nameof(UV2));
+				UInt_0C = s.Serialize<uint>(UInt_0C, name: nameof(UInt_0C));
+				UInt_10 = s.Serialize<uint>(UInt_10, name: nameof(UInt_10));
+			}
+		}
+
+		public class UnknownStruct : R1Serializable {
+			public uint UInt_00 { get; set; }
+			public uint UInt_04 { get; set; }
+			public uint UInt_Editor_08 { get; set; }
+
+			public override void SerializeImpl(SerializerObject s) {
+				UInt_00 = s.Serialize<uint>(UInt_00, name: nameof(UInt_00));
+				UInt_04 = s.Serialize<uint>(UInt_04, name: nameof(UInt_04));
+				UInt_Editor_08 = s.Serialize<uint>(UInt_Editor_08, name: nameof(UInt_Editor_08));
+			}
+		}
+	}
+}
