112ad8db4b6b07d16e250a5b3c18dc601c1a7c2b
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-04-01 09:37:51 +0200

    Jade: Parse some more event data

---
+++ Assets/Scripts/DataTypes/Jade/EVE/EVE_Event.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using BinarySerializer;
+
+namespace R1Engine.Jade
+{
+    public class EVE_Event : BinarySerializable
+    {
+        public EVE_ListEvents ListEvents { get; set; } // Set before serializing
+        public int Index { get; set; } // Set before serializing
+
+        public ushort Ushort_00 { get; set; }
+        public ushort Ushort_02 { get; set; }
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            LOA_Loader Loader = Context.GetStoredObject<LOA_Loader>(Jade_BaseManager.LoaderKey);
+
+            if (ListEvents.Track.Flags.HasFlag(EVE_Track.TrackFlags.Flag_9))
+                Ushort_00 = s.Serialize<byte>((byte)Ushort_00, name: nameof(Ushort_00));
+            else
+                Ushort_00 = s.Serialize<ushort>(Ushort_00, name: nameof(Ushort_00));
+
+            if (ListEvents.Track.Flags.HasFlag(EVE_Track.TrackFlags.Flag_11) && Index != 0)
+            {
+                if (ListEvents.Track.Uint_04 > 0 &&
+                    ListEvents.Track.Flags.HasFlag(EVE_Track.TrackFlags.Flag_11) &&
+                    ListEvents.Track.Flags.HasFlag(EVE_Track.TrackFlags.Flag_12) &&
+                    ListEvents.Track.Flags.HasFlag(EVE_Track.TrackFlags.Flag_13) &&
+                    (ListEvents.Header_Flags & 0x80) != 0)
+                    Ushort_02 = 128;
+                else
+                    throw new NotImplementedException($"TODO: Implement {GetType()}"); // Ushort_02 = *(_WORD *)(*(_DWORD *)(trackStruct + 4) + 2);
+
+                Ushort_02 &= 0xFFFB;
+            }
+            else
+            {
+                Ushort_02 = s.Serialize<ushort>(Ushort_02, name: nameof(Ushort_02));
+            }
+
+            var v24 = Ushort_02 & 0x7C0;
+
+            throw new NotImplementedException($"TODO: Implement {GetType()}");
+        }
+    }
+}
--- Assets/Scripts/DataTypes/Jade/EVE/EVE_ListEvents.cs
+++ Assets/Scripts/DataTypes/Jade/EVE/EVE_ListEvents.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using BinarySerializer;
+﻿using BinarySerializer;
 
 namespace R1Engine.Jade
 {
@@ -8,20 +7,56 @@ namespace R1Engine.Jade
         public EVE_Track Track { get; set; } // Set before serializing
         public uint EventsCount { get; set; }
 
+        public short Header_Short_00 { get; set; }
+        public short Header_Short_02 { get; set; }
+        public ushort Header_Ushort_04 { get; set; }
+        public ushort Header_Flags { get; set; }
+        public byte[] Header_Bytes { get; set; }
+        public int Header_Int { get; set; }
+
+        public EVE_Event[] Events { get; set; }
+
         public override void SerializeImpl(SerializerObject s)
         {
-            LOA_Loader Loader = Context.GetStoredObject<LOA_Loader>(Jade_BaseManager.LoaderKey);
-
             EventsCount = s.Serialize<uint>(EventsCount, name: nameof(EventsCount));
 
             if (Track.Uint_04 > 0)
             {
-                // TODO: Read data
+                if (Track.Flags.HasFlag(EVE_Track.TrackFlags.Flag_9))
+                    Header_Short_00 = s.Serialize<byte>((byte)Header_Short_00, name: nameof(Header_Short_00));
+                else
+                    Header_Short_00 = s.Serialize<short>(Header_Short_00, name: nameof(Header_Short_00));
+
+                Header_Short_02 = s.Serialize<short>(Header_Short_02, name: nameof(Header_Short_02));
+                Header_Ushort_04 = s.Serialize<ushort>(Header_Ushort_04, name: nameof(Header_Ushort_04));
+                Header_Flags = s.Serialize<ushort>(Header_Flags, name: nameof(Header_Flags));
+
+                if (Track.Flags.HasFlag(EVE_Track.TrackFlags.Flag_13) && (Header_Flags & 8) != 0)
+                {
+                    long count = Header_Ushort_04;
+
+                    if ((Header_Flags & 0x10) != 0 && (Header_Flags & 0x80) != 0)
+                        count = Track.Flags.HasFlag(EVE_Track.TrackFlags.Flag_12) ? 6 : 16;
+
+                    if ((Header_Flags & 3) != 0)
+                        count = 12 * (Header_Flags & 3);
+
+                    Header_Bytes = s.SerializeArray(Header_Bytes, count, name: nameof(Header_Bytes));
+                    Header_Int = s.Serialize<int>(Header_Int, name: nameof(Header_Int));
+                }
             }
 
-            // TODO: Read event array
+            if (Events == null)
+                Events = new EVE_Event[EventsCount];
 
-            throw new NotImplementedException($"TODO: Implement {GetType()}");
+            for (int i = 0; i < Events.Length; i++)
+            {
+                Events[i] = s.SerializeObject(Events[i], x =>
+                {
+                    x.ListEvents = this;
+                    x.Index = i;
+                }, name: $"{nameof(Events)}[{i}]");
+            }
         }
     }
 }
