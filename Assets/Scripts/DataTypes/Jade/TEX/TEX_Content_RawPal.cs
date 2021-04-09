d9a2d40d99fb720323e9c737bfbc1245c44e8005
byvar <39142447+byvar@users.noreply.github.com>
2021-04-09 19:45:02 +0200

    Jade: Only load first non-null reference in TEX_Content_RawPal

--- Assets/Scripts/DataTypes/Jade/TEX/TEX_Content_RawPal.cs
+++ Assets/Scripts/DataTypes/Jade/TEX/TEX_Content_RawPal.cs
@@ -12,6 +12,12 @@ namespace R1Engine.Jade {
             if (!(Texture.FileSize > 0x50 || Texture.FileSize % 4 != 0)) {
                 var count = (Texture.FileSize - (s.CurrentPointer - Texture.Offset)) / 12;
                 References = s.SerializeObjectArray<TexPaletteReference>(References, count, name: nameof(References));
+                foreach (var reference in References) {
+                    if (!reference.RawTexture.IsNull || !reference.Palette.IsNull || !reference.Unknown.IsNull) {
+                        reference.Resolve();
+                        break;
+                    }
+                }
             }
         }
 
@@ -20,9 +26,14 @@ namespace R1Engine.Jade {
             public Jade_PaletteReference Palette { get; set; }
             public Jade_Key Unknown { get; set; }
             public override void SerializeImpl(SerializerObject s) {
-				RawTexture = s.SerializeObject<Jade_TextureReference>(RawTexture, name: nameof(RawTexture))?.Resolve();
-                Palette = s.SerializeObject<Jade_PaletteReference>(Palette, name: nameof(Palette))?.Resolve();
+				RawTexture = s.SerializeObject<Jade_TextureReference>(RawTexture, name: nameof(RawTexture));
+                Palette = s.SerializeObject<Jade_PaletteReference>(Palette, name: nameof(Palette));
                 Unknown = s.SerializeObject<Jade_Key>(Unknown, name: nameof(Unknown));
+            }
+
+            public void Resolve() {
+                RawTexture?.Resolve();
+                Palette?.Resolve();
             }
 		}
 	}
