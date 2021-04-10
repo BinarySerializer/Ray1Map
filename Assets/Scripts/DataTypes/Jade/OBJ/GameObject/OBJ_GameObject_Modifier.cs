3a5cebb457c001d760d5f38af61c5999bed13daf
byvar <39142447+byvar@users.noreply.github.com>
2021-04-10 04:46:59 +0200

    Jade: Add GEN_ModifierSoundFx

--- Assets/Scripts/DataTypes/Jade/OBJ/GameObject/OBJ_GameObject_Modifier.cs
+++ Assets/Scripts/DataTypes/Jade/OBJ/GameObject/OBJ_GameObject_Modifier.cs
@@ -24,6 +24,7 @@ namespace R1Engine.Jade {
                 MDF_ModifierType.GEO_ModifierMorphing => s.SerializeObject<GEO_ModifierMorphing>((GEO_ModifierMorphing)Modifier, name: nameof(Modifier)),
                 MDF_ModifierType.GEO_ModifierSnap => s.SerializeObject<GEO_ModifierSnap>((GEO_ModifierSnap)Modifier, name: nameof(Modifier)),
                 MDF_ModifierType.GEN_ModifierSound => s.SerializeObject<GEN_ModifierSound>((GEN_ModifierSound)Modifier, name: nameof(Modifier)),
+                MDF_ModifierType.GEN_ModifierSoundFx => s.SerializeObject<GEN_ModifierSoundFx>((GEN_ModifierSoundFx)Modifier, name: nameof(Modifier)),
                 MDF_ModifierType.GAO_ModifierShadow => s.SerializeObject<GAO_ModifierShadow>((GAO_ModifierShadow)Modifier, name: nameof(Modifier)),
                 MDF_ModifierType.PROTEX_Modifier => s.SerializeObject<PROTEX_Modifier>((PROTEX_Modifier)Modifier, name: nameof(Modifier)),
                 MDF_ModifierType.GAO_ModifierLazy => s.SerializeObject<GAO_ModifierLazy>((GAO_ModifierLazy)Modifier, name: nameof(Modifier)),
