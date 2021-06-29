b8d8b816b27f91e19a5fce1c6391a0996322af33
byvar <39142447+byvar@users.noreply.github.com>
2021-06-29 18:18:59 +0200

    Jade: Fully fix PoP:SoT GC

---
+++ Assets/Scripts/DataTypes/Jade/MDF/Modifiers/GAO_ModifierSectorisationElement.cs
@@ -0,0 +1,31 @@
+﻿using BinarySerializer;
+
+namespace R1Engine.Jade {
+	public class GAO_ModifierSectorisationElement : MDF_Modifier {
+		public uint Version { get; set; }
+		public SectoElementType Type { get; set; }
+		public uint Group1 { get; set; }
+		public uint Group2 { get; set; }
+		public uint Flags { get; set; }
+		public Jade_Reference<OBJ_World_GroupObjectList> Group { get; set; }
+
+		public override void SerializeImpl(SerializerObject s) {
+			Version = s.Serialize<uint>(Version, name: nameof(Version));
+			if (Version >= 1) {
+				Type = s.Serialize<SectoElementType>(Type, name: nameof(Type));
+				Group1 = s.Serialize<uint>(Group1, name: nameof(Group1));
+				Group2 = s.Serialize<uint>(Group2, name: nameof(Group2));
+				Flags = s.Serialize<uint>(Flags, name: nameof(Flags));
+				if (Type == SectoElementType.Sector && Version >= 2) {
+					Group = s.SerializeObject<Jade_Reference<OBJ_World_GroupObjectList>>(Group, name: nameof(Group))?.Resolve(onPreSerialize: (_, gol) => gol.ResolveObjects = false);
+				}
+			}
+		}
+
+		public enum SectoElementType : uint {
+			Sector = 0,
+			Door = 1,
+			Occluder = 2
+		}
+	}
+}
--- Assets/Scripts/DataTypes/Jade/OBJ/OBJ_World_GroupObjectList.cs
+++ Assets/Scripts/DataTypes/Jade/OBJ/OBJ_World_GroupObjectList.cs
@@ -5,15 +5,18 @@ namespace R1Engine.Jade
     public class OBJ_World_GroupObjectList : Jade_File 
     {
 		public GroupObject[] GroupObjects { get; set; }
+        public bool ResolveObjects { get; set; } = true;
 
 		public override void SerializeImpl(SerializerObject s) 
         {
 			var fileEnd = Offset + FileSize;
-			GroupObjects = s.SerializeObjectArrayUntil(GroupObjects, x => s.CurrentAbsoluteOffset >= fileEnd.AbsoluteOffset, name: nameof(GroupObjects));
+			GroupObjects = s.SerializeObjectArrayUntil(GroupObjects, x => s.CurrentAbsoluteOffset >= fileEnd.AbsoluteOffset, onPreSerialize: go => go.List = this, name: nameof(GroupObjects));
 		}
 
         public class GroupObject : BinarySerializable
         {
+            public OBJ_World_GroupObjectList List { get; set; }
+
             public Jade_Reference<OBJ_GameObject> GameObject { get; set; }
             public uint Uint_04_Editor { get; set; }
 
@@ -21,7 +24,8 @@ namespace R1Engine.Jade
             {
                 LOA_Loader Loader = Context.GetStoredObject<LOA_Loader>(Jade_BaseManager.LoaderKey);
 
-                GameObject = s.SerializeObject<Jade_Reference<OBJ_GameObject>>(GameObject, name: nameof(GameObject))?.Resolve();
+                GameObject = s.SerializeObject<Jade_Reference<OBJ_GameObject>>(GameObject, name: nameof(GameObject));
+                if(List.ResolveObjects) GameObject?.Resolve();
                 if (!Loader.IsBinaryData) Uint_04_Editor = s.Serialize<uint>(Uint_04_Editor, name: nameof(Uint_04_Editor));
             }
         }
