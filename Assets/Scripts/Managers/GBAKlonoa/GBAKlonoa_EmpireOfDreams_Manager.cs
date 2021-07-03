a387a49e0521e67d8e59e3f6bf56e7cb2fb357df
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-07-03 21:25:02 +0200

    Klonoa: Fix map tiles which use multiple palettes

--- Assets/Scripts/Managers/GBAKlonoa/GBAKlonoa_EmpireOfDreams_Manager.cs
+++ Assets/Scripts/Managers/GBAKlonoa/GBAKlonoa_EmpireOfDreams_Manager.cs
@@ -44,31 +44,11 @@ namespace R1Engine
             Controller.DetailedState = "Loading maps";
             await Controller.WaitIfNecessary();
 
-            var pal_8 = Util.ConvertGBAPalette(rom.MapPalettes[globalLevelIndex]);
-            var pal_4 = Util.ConvertAndSplitGBAPalette(rom.MapPalettes[globalLevelIndex]);
-
             var maps = Enumerable.Range(0, 3).Select(mapIndex =>
             {
                 var width = rom.MapWidths[globalLevelIndex].Widths[mapIndex];
                 var is8Bit = mapIndex == 2;
                 var map = rom.Maps[globalLevelIndex].Maps[mapIndex];
-
-                Func<int, Color[]> getPalFunc = null;
-
-                if (!is8Bit)
-                {
-                    Dictionary<int, int> tilesetPalettes = new Dictionary<int, int>();
-
-                    foreach (var m in map)
-                    {
-                        if (!tilesetPalettes.ContainsKey(m.TileMapY))
-                            tilesetPalettes[m.TileMapY] = m.PaletteIndex;
-                        else if (tilesetPalettes[m.TileMapY] != m.PaletteIndex && m.TileMapY != 0)
-                            Debug.LogWarning($"Tile {m.TileMapY} has several possible palettes: {tilesetPalettes[m.TileMapY]} - {m.PaletteIndex}");
-                    }
-
-                    getPalFunc = x => pal_4[tilesetPalettes.TryGetItem(x)];
-                }
 
                 var imgData = rom.TileSets[globalLevelIndex].TileSets[mapIndex];
 
@@ -82,13 +62,7 @@ namespace R1Engine
                     Height = (ushort)(map.Length / width),
                     TileSet = new Unity_TileSet[]
                     {
-                        new Unity_TileSet(Util.ToTileSetTexture(
-                            imgData: imgData,
-                            pal: pal_8,
-                            encoding: is8Bit ? Util.TileEncoding.Linear_8bpp : Util.TileEncoding.Linear_4bpp,
-                            tileWidth: CellSize, 
-                            flipY: false, 
-                            getPalFunc: getPalFunc), CellSize)
+                        LoadTileSet(imgData, rom.MapPalettes[globalLevelIndex], is8Bit, map)
                     },
                     MapTiles = map.Select(x => new Unity_Tile(x)).ToArray(),
                     Type = Unity_Map.MapType.Graphics,
@@ -192,6 +166,92 @@ namespace R1Engine
                 cellSize: CellSize,
                 defaultLayer: 2,
                 isometricData: isMap ? Unity_IsometricData.Mode7(CellSize) : null);
+        }
+
+        public Unity_TileSet LoadTileSet(byte[] tileSet, RGBA5551Color[] pal, bool is8bit, MapTile[] mapTiles_4)
+        {
+            Texture2D tex;
+            var additionalTiles = new List<Texture2D>();
+            var tileSize = is8bit ? 0x40 : 0x20;
+            var paletteIndices = Enumerable.Range(0, tileSet.Length / tileSize).Select(x => new List<byte>()).ToArray();
+            var tilesCount = tileSet.Length / tileSize;
+
+            if (is8bit)
+            {
+                tex = Util.ToTileSetTexture(tileSet, Util.ConvertGBAPalette(pal), Util.TileEncoding.Linear_8bpp, CellSize, false);
+            }
+            else
+            {
+                var palettes = Util.ConvertAndSplitGBAPalette(pal);
+
+                foreach (var m in mapTiles_4)
+                {
+                    if (m.TileMapY < paletteIndices.Length && !paletteIndices[m.TileMapY].Contains(m.PaletteIndex))
+                        paletteIndices[m.TileMapY].Add(m.PaletteIndex);
+                }
+
+                tex = Util.ToTileSetTexture(tileSet, palettes[0], Util.TileEncoding.Linear_4bpp, CellSize, false, getPalFunc: x =>
+                {
+                    var p = paletteIndices[x].ElementAtOrDefault(0);
+                    return palettes[p];
+                });
+
+                // Add additional tiles for tiles with multiple palettes
+                for (int tileIndex = 0; tileIndex < paletteIndices.Length; tileIndex++)
+                {
+                    for (int palIndex = 1; palIndex < paletteIndices[tileIndex].Count; palIndex++)
+                    {
+                        var p = paletteIndices[tileIndex][palIndex];
+
+                        var tileTex = TextureHelpers.CreateTexture2D(CellSize, CellSize);
+
+                        // Create a new tile
+                        tileTex.FillInTile(
+                            imgData: tileSet,
+                            imgDataOffset: tileSize * tileIndex,
+                            pal: palettes[p],
+                            encoding: Util.TileEncoding.Linear_4bpp,
+                            tileWidth: CellSize,
+                            flipTextureY: false,
+                            tileX: 0,
+                            tileY: 0);
+
+                        // Modify all tiles where this is used
+                        foreach (MapTile t in mapTiles_4.Where(x => x.TileMapY == tileIndex && x.PaletteIndex == p))
+                        {
+                            t.TileMapY = (ushort)(tilesCount + additionalTiles.Count);
+                        }
+
+                        // Add to additional tiles list
+                        additionalTiles.Add(tileTex);
+                    }
+                }
+            }
+
+            // Create the tile array
+            var tiles = new Unity_TileTexture[tilesCount + additionalTiles.Count];
+
+            // Keep track of the index
+            var index = 0;
+
+            // Add every normal tile
+            for (int y = 0; y < tex.height; y += CellSize)
+            {
+                for (int x = 0; x < tex.width; x += CellSize)
+                {
+                    if (index >= tilesCount)
+                        break;
+
+                    // Create a tile
+                    tiles[index++] = tex.CreateTile(new Rect(x, y, CellSize, CellSize));
+                }
+            }
+
+            // Add additional tiles
+            foreach (Texture2D t in additionalTiles)
+                tiles[index++] = t.CreateTile();
+
+            return new Unity_TileSet(tiles);
         }
 
         public Texture2D[] GetAnimFrames(GBAKlonoa_AnimationFrame[] frames, GBAKlonoa_ObjectOAMCollection oamCollection, Color[][] palettes, int imgDataOffset = 0)
