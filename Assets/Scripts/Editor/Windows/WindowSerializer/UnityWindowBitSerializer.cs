11cec073a2381298d2145e2cfe404b0d8cd640c1
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-04-28 22:27:52 +0200

    Update BinarySerializer

--- Assets/Scripts/Editor/Windows/WindowSerializer/UnityWindowBitSerializer.cs
+++ Assets/Scripts/Editor/Windows/WindowSerializer/UnityWindowBitSerializer.cs
@@ -5,7 +5,7 @@ public class UnityWindowBitSerializer : BitSerializerObject
     public UnityWindowBitSerializer(SerializerObject serializerObject, Pointer valueOffset, string logPrefix, long value) 
         : base(serializerObject, valueOffset, logPrefix, value) { }
 
-    public override T SerializeBits<T>(T value, int length, string name = null) 
+    public override T SerializeBits<T>(T value, int length, SignedNumberRepresentation sign = SignedNumberRepresentation.Unsigned, string name = null) 
     {
         T t = SerializerObject.Serialize<T>(value, name);
 
