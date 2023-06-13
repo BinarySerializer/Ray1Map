6cd429abd4d40eb0a4edeb8fc9bcf2c687d640f7
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-06-13 20:18:37 +0200

    Improved VersionTree creation syntax

--- Assets/Scripts/Settings/EngineVersionTree.cs
+++ Assets/Scripts/Settings/EngineVersionTree.cs
@@ -15,76 +15,77 @@ namespace Ray1Map
             };
         }
 
-        public static EngineVersionTree Create_Jade(GameSettings settings) {
-            EngineVersionTree tree = new EngineVersionTree(
-                root: new Node(EngineVersion.Jade_Engine).SetChildren(
-                    new Node(EngineVersion.Jade_Montpellier).SetChildren(
-                        new Node(EngineVersion.Jade_BGE).SetChildren(
-                            new Node(EngineVersion.Jade_BGE_HD),
-                            new Node(EngineVersion.Jade_KingKong).SetChildren(
-                                new Node(EngineVersion.Jade_KingKong_Xenon),
-                                new Node(EngineVersion.Jade_RRRPrototype).SetChildren(
-                                    new Node(EngineVersion.Jade_RRR).SetChildren(
-                                        new Node(EngineVersion.Jade_RRR2),
-                                        new Node(EngineVersion.Jade_Phoenix).SetChildren(
-                                            new Node(EngineVersion.Jade_Horsez2).SetChildren(
-                                                new Node(EngineVersion.Jade_PetzHorseClub).SetChildren(
-                                                    new Node(EngineVersion.Jade_MovieGames)
-                                                )
-                                            )
-                                        )
-                                    )
-                                )
-                            )
-                        )
-                    ),
-                    new Node(EngineVersion.Jade_Montreal).SetChildren(
-                        new Node(EngineVersion.Jade_PoP_SoT_20030723).SetChildren(
-                            new Node(EngineVersion.Jade_PoP_SoT_20030819).SetChildren(
-                                new Node(EngineVersion.Jade_PoP_SoT).SetChildren(
-                                    new Node(EngineVersion.Jade_PoP_WW_20040920).SetChildren(
-                                        new Node(EngineVersion.Jade_PoP_WW_20041024).SetChildren(
-                                            new Node(EngineVersion.Jade_PoP_WW).SetChildren(
-                                                new Node(EngineVersion.Jade_PhoenixRayman4).SetChildren(
-                                                    new Node(EngineVersion.Jade_Horsez)
-                                                ),
-                                                new Node(EngineVersion.Jade_Beowulf),
-                                                new Node(EngineVersion.Jade_PoP_T2T_20051002).SetChildren(
-                                                    new Node(EngineVersion.Jade_PoP_T2T).SetChildren( // BIG: 0x26
-														new Node(EngineVersion.Jade_TMNT).SetChildren( // BIG: 0x2A
-															new Node(EngineVersion.Jade_MyWordCoach), // BIG: 0x2A
-															new Node(EngineVersion.Jade_PostTMNT).SetChildren(
-                                                                new Node(EngineVersion.Jade_CPP).SetChildren( // bf name format: [game]_bin_[platform].bf
-																    new Node(EngineVersion.Jade_RRRTVParty).SetChildren( // BIG: 0x2C
-																	    new Node(EngineVersion.Jade_JustDance)
-																    ),
-																    new Node(EngineVersion.Jade_PoP_TFS).SetChildren( // BIG: 0x2A
-																		new Node(EngineVersion.Jade_Avatar).SetChildren( // BIG: 0x2A
-																			new Node(EngineVersion.Jade_SeanWhiteSkateboarding) // BIG: 0x2A (but BF64!)
-																		),
-																		new Node(EngineVersion.Jade_PoP_TFS_PSP) // BIG: 0x2A
-																	),
-																    new Node(EngineVersion.Jade_Fox).SetChildren( // BIG: 0x2A
-																	    new Node(EngineVersion.Jade_Naruto1RiseOfANinja).SetChildren( // BIG : 0x2A
-																		    new Node(EngineVersion.Jade_Naruto2TheBrokenBond)
-																	    )
-																    )
-																),
-                                                                new Node(EngineVersion.Jade_CloudyWithAChanceOfMeatballs).SetChildren( // BIG: 0x2B
-                                                                    new Node(EngineVersion.Jade_NCIS)
-                                                                )
-                                                            )
-                                                        )
-                                                    )
-                                                )
-                                            )
-                                        )
-                                    )
-                                )
-                            )
-                        )
-                    )
-                ));
+        public static EngineVersionTree Create_Jade(GameSettings settings) 
+        {
+            EngineVersionTree tree = new(
+                new Node(EngineVersion.Jade_Engine) {
+                    new(EngineVersion.Jade_Montpellier) {
+                        new(EngineVersion.Jade_BGE) {
+                            new(EngineVersion.Jade_BGE_HD),
+                            new(EngineVersion.Jade_KingKong) {
+                                new(EngineVersion.Jade_KingKong_Xenon),
+                                new(EngineVersion.Jade_RRRPrototype) {
+                                    new(EngineVersion.Jade_RRR) {
+                                        new(EngineVersion.Jade_RRR2),
+                                        new(EngineVersion.Jade_Phoenix) {
+                                            new(EngineVersion.Jade_Horsez2) {
+                                                new(EngineVersion.Jade_PetzHorseClub) {
+                                                    new(EngineVersion.Jade_MovieGames)
+                                                },
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    new(EngineVersion.Jade_Montreal) {
+                        new(EngineVersion.Jade_PoP_SoT_20030723) {
+                            new(EngineVersion.Jade_PoP_SoT_20030819) {
+                                new(EngineVersion.Jade_PoP_SoT) {
+                                    new(EngineVersion.Jade_PoP_WW_20040920) {
+                                        new(EngineVersion.Jade_PoP_WW_20041024) {
+                                            new(EngineVersion.Jade_PoP_WW) {
+                                                new(EngineVersion.Jade_PhoenixRayman4) {
+                                                    new(EngineVersion.Jade_Horsez)
+                                                },
+                                                new(EngineVersion.Jade_Beowulf),
+                                                new(EngineVersion.Jade_PoP_T2T_20051002) {
+                                                    new(EngineVersion.Jade_PoP_T2T) { // BIG: 0x26
+                                                        new(EngineVersion.Jade_TMNT) { // BIG: 0x2A
+                                                            new(EngineVersion.Jade_MyWordCoach), // BIG: 0x2A
+                                                            new(EngineVersion.Jade_PostTMNT) {
+                                                                new(EngineVersion.Jade_CPP) { // bf name format: [game]_bin_[platform].bf
+                                                                    new(EngineVersion.Jade_RRRTVParty) { // BIG: 0x2C
+                                                                        new(EngineVersion.Jade_JustDance)
+                                                                    },
+                                                                    new(EngineVersion.Jade_PoP_TFS) { // BIG: 0x2A
+                                                                        new(EngineVersion.Jade_Avatar) { // BIG: 0x2A
+                                                                            new(EngineVersion.Jade_SeanWhiteSkateboarding) // BIG: 0x2A (but BF64!)
+                                                                        },
+                                                                        new(EngineVersion.Jade_PoP_TFS_PSP) // BIG: 0x2A
+                                                                    },
+                                                                    new(EngineVersion.Jade_Fox) { // BIG: 0x2A
+                                                                        new(EngineVersion.Jade_Naruto1RiseOfANinja) { // BIG : 0x2A
+                                                                            new(EngineVersion.Jade_Naruto2TheBrokenBond)
+                                                                        },
+                                                                    }
+                                                                },
+                                                                new(EngineVersion.Jade_CloudyWithAChanceOfMeatballs) { // BIG: 0x2B
+                                                                    new(EngineVersion.Jade_NCIS)
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                });
             tree.Init();
             tree.Current = tree.FindVersion(settings.EngineVersion);
             
